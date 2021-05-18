a1ec42c8dfc8f02d48fb888a9323013d036575ea
Nicolas Marcq <nico.marcq@gmail.com>
2021-05-18 14:19:22 +0200

    add customer tests on instance

--- service_catalog/models/__init__.py
+++ service_catalog/models/__init__.py
@@ -4,6 +4,6 @@ from service_catalog.models.services import Service
 from service_catalog.models.operations import Operation
 from service_catalog.models.request import Request
 from service_catalog.models.services import Service
-from service_catalog.models.instance import Instance, UserPermissionOnInstance
+from service_catalog.models.instance import Instance
 from service_catalog.models.message import Message, RequestMessage, SupportMessage
 from service_catalog.models.support import Support
--- tests/base_test_request.py
+++ tests/base_test_request.py
@@ -1,4 +1,6 @@
-from service_catalog.models import Request, Instance
+from guardian.mixins import UserObjectPermission
+
+from service_catalog.models import Request, Instance, Support
 from tests.base import BaseTest
 
 
@@ -8,7 +10,19 @@ class BaseTestRequest(BaseTest):
         super(BaseTestRequest, self).setUp()
         form_data = {'instance_name': 'test instance', 'text_variable': 'my_var'}
         self.test_instance = Instance.objects.create(name="test_instance_1", service=self.service_test)
+        # give user perm on this instance
+        UserObjectPermission.objects.assign_perm('change_instance', self.standard_user, obj=self.test_instance)
+        UserObjectPermission.objects.assign_perm('view_instance', self.standard_user, obj=self.test_instance)
+        # add a first request
         self.test_request = Request.objects.create(fill_in_survey=form_data,
                                                    instance=self.test_instance,
                                                    operation=self.create_operation_test,
                                                    user=self.standard_user)
+
+        self.support_test = Support.objects.create(title="support_1", instance=self.test_instance)
+
+        # second instance for second user
+        self.test_instance_2 = Instance.objects.create(name="test_instance_2", service=self.service_test)
+        # give user perm on this instance
+        UserObjectPermission.objects.assign_perm('change_instance', self.standard_user_2, obj=self.test_instance_2)
+        UserObjectPermission.objects.assign_perm('view_instance', self.standard_user_2, obj=self.test_instance_2)
