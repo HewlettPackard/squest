c48eb9dd50eae310d33820a0e76982eae62050e5
Nicolas Marcq <nico.marcq@gmail.com>
2021-06-03 16:33:34 +0200

    add Swagger API doc

--- Squest/urls.py
+++ Squest/urls.py
@@ -13,15 +13,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
+#  drf-yasg
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Squest API",
+      default_version='v1',
+      description="Test description",
+      license=openapi.License(name="Apache-2.0 License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('service_catalog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('api/', include('service_catalog.api.urls')),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 # if settings.DEBUG:
