68cbd9bf0d8f835b5a4d59d81f63ce8ce4127a74
Nicolas Marcq <nico.marcq@gmail.com>
2021-06-07 16:18:13 +0200

    add delete attribute view

--- resource_tracker/urls.py
+++ resource_tracker/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     path('resource_group/resource_group/<int:resource_group_id>/attributes/<int:attribute_id>',
          views.resource_group_attribute_edit,
          name='resource_group_attribute_edit'),
+    path('resource_group/resource_group/<int:resource_group_id>/attributes/<int:attribute_id>/delete',
+         views.resource_group_attribute_delete,
+         name='resource_group_attribute_delete'),
 ]
