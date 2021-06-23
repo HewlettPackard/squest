e2ec505ebc5ae4cbb19a4a34da70967d74d0b9f9
Nicolas Marcq <nico.marcq@gmail.com>
2021-06-23 15:28:16 +0200

    fix resource form create and update

--- resource_tracker/views.py
+++ resource_tracker/views.py
@@ -25,6 +25,18 @@ def resource_group_edit(request, resource_group_id):
 
 
 @user_passes_test(lambda u: u.is_superuser)
+def resource_group_create(request):
+    if request.method == 'POST':
+        form = ResourceGroupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(resource_group_list)
+    else:
+        form = ResourceGroupForm()
+    return render(request, 'resource_tracking/resource_group/resource-group-create.html', {'form': form})
+
+
+@user_passes_test(lambda u: u.is_superuser)
 def resource_group_delete(request, resource_group_id):
     resource_group = get_object_or_404(ResourceGroup, id=resource_group_id)
     if request.method == 'POST':
@@ -62,18 +74,6 @@ def resource_group_attribute_create(request, resource_group_id):
     return render(request,
                   'resource_tracking/resource_group/attributes/attribute-create.html',
                   {'form': form, 'resource_group': resource_group})
-
-
-@user_passes_test(lambda u: u.is_superuser)
-def resource_group_create(request):
-    if request.method == 'POST':
-        form = ResourceGroupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(resource_group_list)
-    else:
-        form = ResourceGroupForm()
-    return render(request, 'resource_tracking/resource_group/resource-group-create.html', {'form': form})
 
 
 @user_passes_test(lambda u: u.is_superuser)
