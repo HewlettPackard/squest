3ed2d4ec049083a6cac13d0ef2a9efe9969a41b7
Nicolas Marcq <nico.marcq@gmail.com>
2021-06-30 15:20:56 +0200

    refactor resource group list view

--- resource_tracker/views/resource_group_attribute_views.py
+++ resource_tracker/views/resource_group_attribute_views.py
@@ -6,13 +6,6 @@ from resource_tracker.models import ResourceGroup, ResourceGroupAttributeDefinit
 
 
 @user_passes_test(lambda u: u.is_superuser)
-def resource_group_attribute_list(request, resource_group_id):
-    resource_group = get_object_or_404(ResourceGroup, id=resource_group_id)
-    return render(request, 'resource_tracking/resource_group/attributes/attribute-list.html',
-                  {'resource_group': resource_group})
-
-
-@user_passes_test(lambda u: u.is_superuser)
 def resource_group_attribute_create(request, resource_group_id):
     resource_group = get_object_or_404(ResourceGroup, id=resource_group_id)
     if request.method == 'POST':
@@ -22,7 +15,8 @@ def resource_group_attribute_create(request, resource_group_id):
             new_attribute = form.save()
             new_attribute.resource_group_definition = resource_group
             new_attribute.save()
-            return redirect(resource_group_attribute_list, resource_group.id)
+            from resource_tracker.views import resource_group_edit
+            return redirect(resource_group_edit, resource_group.id)
     else:
         form = ResourceGroupAttributeDefinitionForm()
 
@@ -38,7 +32,8 @@ def resource_group_attribute_edit(request, resource_group_id, attribute_id):
     form = ResourceGroupAttributeDefinitionForm(request.POST or None, instance=attribute)
     if form.is_valid():
         form.save()
-        return redirect(resource_group_attribute_list, attribute.resource_group_definition.id)
+        from resource_tracker.views import resource_group_edit
+        return redirect(resource_group_edit, resource_group.id)
     return render(request, 'resource_tracking/resource_group/attributes/attribute-edit.html',
                   {'form': form, 'attribute': attribute, 'resource_group': resource_group})
 
@@ -49,7 +44,8 @@ def resource_group_attribute_delete(request, resource_group_id, attribute_id):
     attribute = get_object_or_404(ResourceGroupAttributeDefinition, id=attribute_id)
     if request.method == "POST":
         attribute.delete()
-        return redirect(resource_group_attribute_list, attribute.resource_group_definition.id)
+        from resource_tracker.views import resource_group_edit
+        return redirect(resource_group_edit, resource_group.id)
     context = {
         "resource_group": resource_group,
         "attribute": attribute
