d3fb688a113426a9a0e2e3b454c6e23a83cb3687
eliasboulharts <boulharts.elias@gmail.com>
2021-07-19 15:33:02 +0200

    Crud for billing groups

--- tests/test_group/test_group_base.py
+++ tests/test_group/test_group_base.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import Group, User
 
+from service_catalog.models import BillingGroup
 from tests.base import BaseTest
 
 
@@ -8,9 +9,11 @@ class TestGroupBase(BaseTest):
     def setUp(self):
         super(TestGroupBase, self).setUp()
         self.my_group = Group.objects.create(name='test_group')
+        self.my_billing_group = BillingGroup.objects.create(name='test_billing_group')
         self.my_user = User.objects.create(username='test_user')
         self.my_user2 = User.objects.create(username='test_user2')
         self.my_user3 = User.objects.create(username='test_user3')
         self.my_user4 = User.objects.create(username='test_user4')
         self.my_group.user_set.add(self.my_user)
         self.my_group.user_set.add(self.my_user3)
+        self.my_billing_group.user_set.add(self.my_user2)
