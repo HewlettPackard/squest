5e9f6e0c258aaff90d18d6c64458783ff058c9da
eliasboulharts <boulharts.elias@gmail.com>
2021-07-23 20:31:10 +0200

    Create service form update with billing groups

--- service_catalog/models/services.py
+++ service_catalog/models/services.py
@@ -5,3 +5,6 @@ class Service(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=500, blank=True)
     image = models.ImageField(upload_to='service_image', blank=True)
+    billing_group_id = models.IntegerField(null=True)
+    billing_group_is_displayed = models.BooleanField(default=False)
+    is_restricted_billing_groups = models.BooleanField(default=True)
