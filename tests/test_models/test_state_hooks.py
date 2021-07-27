791dcd8b9040d4de03f2f88b6c0849eb3f091c10
Nicolas Marcq <nico.marcq@gmail.com>
2021-07-27 17:13:00 +0200

    state hook now send instance or request data to the executed job

--- tests/test_models/test_state_hooks.py
+++ tests/test_models/test_state_hooks.py
@@ -33,15 +33,21 @@ class TestStateHook(BaseTestRequest):
             self.assertEquals(mock_trigger_hook.call_count, 2)
 
     def test_hook_manager_execute_job_template(self):
+        from service_catalog.serializers.instance_serializer import InstanceSerializer
+        from service_catalog.serializers.request_serializer import RequestSerializer
         with mock.patch("service_catalog.models.job_templates.JobTemplate.execute") as mock_job_template_execute:
 
             HookManager.trigger_hook(sender=Request, instance=self.test_request,
                                      name="accept", source="SUBMITTED", target="ACCEPTED")
-            mock_job_template_execute.assert_called_with(extra_vars=self.global_hook1.extra_vars)
+            expected_extra_vars = self.global_hook1.extra_vars
+            expected_extra_vars["squest"] = RequestSerializer(self.test_request).data
+            mock_job_template_execute.assert_called_with(extra_vars=expected_extra_vars)
 
             HookManager.trigger_hook(sender=Instance, instance=self.test_instance,
                                      name="accept", source="PENDING", target="PROVISIONING")
-            mock_job_template_execute.assert_called_with(extra_vars=self.global_hook2.extra_vars)
+            expected_extra_vars = self.global_hook2.extra_vars
+            expected_extra_vars["squest"] = InstanceSerializer(self.test_instance).data
+            mock_job_template_execute.assert_called_with(extra_vars=expected_extra_vars)
 
     def test_hook_manager_does_not_execute_job_template(self):
         with mock.patch("service_catalog.models.job_templates.JobTemplate.execute") as mock_job_template_execute:
