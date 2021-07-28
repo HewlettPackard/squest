88b2190442242a6e9eb320d905a086deef9ad2e9
Nicolas Marcq <nico.marcq@gmail.com>
2021-07-28 10:33:29 +0200

    code review fix

--- tests/test_views/admin/settings/global_hooks/test_create.py
+++ tests/test_views/admin/settings/global_hooks/test_create.py
@@ -36,7 +36,7 @@ class GlobalHooksCreateViewsTest(BaseTest):
         self.assertEquals(self.number_global_hook_before, GlobalHook.objects.all().count())
         self.assertContains(response, "Select a valid choice", status_code=200, html=False)
 
-    def test_create_invalid_state(self):
+    def test_create_valid_model_invalid_state(self):
         data = {
             "name": "hook1",
             "model": "Instance",
@@ -48,3 +48,28 @@ class GlobalHooksCreateViewsTest(BaseTest):
         self.assertEquals(200, response.status_code)
         self.assertEquals(self.number_global_hook_before, GlobalHook.objects.all().count())
         self.assertContains(response, "Select a valid choice", status_code=200, html=False)
+
+    def test_create_valid_model_valid_state_from_other_model(self):
+        data1 = {
+            "name": "hook1",
+            "model": "Instance",
+            "state": "ACCEPTED",
+            "job_template": self.job_template_test.id,
+            "extra_vars": "{}"
+        }
+
+        data2 = {
+            "name": "hook2",
+            "model": "Request",
+            "state": "PENDING",
+            "job_template": self.job_template_test.id,
+            "extra_vars": "{}"
+        }
+
+        to_be_tested = [data1, data2]
+
+        for data in to_be_tested:
+            response = self.client.post(self.url, data=data)
+            self.assertEquals(200, response.status_code)
+            self.assertEquals(self.number_global_hook_before, GlobalHook.objects.all().count())
+            self.assertContains(response, "is not a valid state of model", status_code=200, html=False)
