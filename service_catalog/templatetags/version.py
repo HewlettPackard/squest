c5842ce049cf6ecac12dbd4ec961758067d7d27b
Nicolas Marcq <nico.marcq@gmail.com>
2021-07-29 11:10:20 +0200

    add footer

---
+++ service_catalog/templatetags/version.py
@@ -0,0 +1,12 @@
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def app_version():
+    """
+    Return Squest version as listed in `__version__` in `init.py` of settings package
+    """
+    from Squest.settings import __version__
+    return __version__
