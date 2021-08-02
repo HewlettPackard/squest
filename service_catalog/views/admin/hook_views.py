b99cfe6225b3f75de5daa56d401d9ba32e98b028
eliasboulharts <boulharts.elias@gmail.com>
2021-08-02 14:56:28 +0200

    Breadcrumbs for tower/awx pages, groups/billing groups pages and global hooks pages

--- service_catalog/views/admin/hook_views.py
+++ service_catalog/views/admin/hook_views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 
 from service_catalog.forms import GlobalHookForm
 from service_catalog.models import GlobalHook
@@ -22,7 +23,12 @@ def global_hook_create(request):
             return redirect('service_catalog:global_hook_list')
     else:
         form = GlobalHookForm()
-    return render(request, 'service_catalog/settings/global_hooks/global-hook-create.html', {'form': form})
+    breadcrumbs = [
+        {'text': 'Global hooks', 'url': reverse('service_catalog:global_hook_list')},
+        {'text': "Create a new global hook", 'url': ""}
+    ]
+    context = {'form': form, 'breadcrumbs': breadcrumbs}
+    return render(request, 'service_catalog/settings/global_hooks/global-hook-create.html', context)
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -32,20 +38,30 @@ def global_hook_edit(request, global_hook_id):
     if form.is_valid():
         form.save()
         return redirect('service_catalog:global_hook_list')
+    breadcrumbs = [
+        {'text': 'Global hooks', 'url': reverse('service_catalog:global_hook_list')},
+        {'text': target_global_hook.name, 'url': ""}
+    ]
     context = {'form': form,
-               'global_hook': target_global_hook
+               'global_hook': target_global_hook,
+               'breadcrumbs': breadcrumbs
                }
     return render(request, 'service_catalog/settings/global_hooks/global-hook-edit.html', context=context)
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def global_hook_delete(request, global_hook_id):
-    obj = get_object_or_404(GlobalHook, id=global_hook_id)
+    target_global_hook = get_object_or_404(GlobalHook, id=global_hook_id)
     if request.method == "POST":
-        obj.delete()
+        target_global_hook.delete()
         return redirect('service_catalog:global_hook_list')
+    breadcrumbs = [
+        {'text': 'Global hooks', 'url': reverse('service_catalog:global_hook_list')},
+        {'text': target_global_hook.name, 'url': ""}
+    ]
     context = {
-        "object": obj
+        'global_hook': target_global_hook,
+        'breadcrumbs': breadcrumbs
     }
     return render(request, "service_catalog/settings/global_hooks/global-hook-delete.html", context)
 
--- service_catalog/views/admin/tower.py
+++ service_catalog/views/admin/tower.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django_celery_results.models import TaskResult
 
 from service_catalog import tasks
@@ -12,7 +13,7 @@ from service_catalog.serializers import TaskResultSerializer
 @permission_required('service_catalog.view_towerserver')
 def list_tower(request):
     tower_servers = TowerServer.objects.all()
-    return render(request, 'service_catalog/settings/tower/tower-list.html', {'tower_servers': tower_servers})
+    return render(request, 'service_catalog/admin/tower/tower-list.html', {'tower_servers': tower_servers})
 
 
 @permission_required('service_catalog.add_towerserver')
@@ -25,7 +26,12 @@ def add_tower(request):
             return redirect('service_catalog:list_tower')
     else:
         form = TowerServerForm()
-    return render(request, 'service_catalog/settings/tower/tower-create.html', {'form': form})
+    breadcrumbs = [
+        {'text': 'Tower/AWX', 'url': reverse('service_catalog:list_tower')},
+        {'text': 'Create a new server', 'url': ""}
+    ]
+    context = {'form': form, 'breadcrumbs': breadcrumbs}
+    return render(request, 'service_catalog/admin/tower/tower-create.html', context)
 
 
 @permission_required('service_catalog.change_towerserver')
@@ -48,28 +54,40 @@ def get_task_result(request, task_id):
 def tower_job_templates_list(request, tower_id):
     tower_server = get_object_or_404(TowerServer, id=tower_id)
     job_templates = JobTemplate.objects.filter(tower_server=tower_server)
+    breadcrumbs = [
+        {'text': 'Tower/AWX', 'url': reverse('service_catalog:list_tower')},
+        {'text': tower_server.name, 'url': ""},
+        {'text': 'Job templates', 'url': ""},
+    ]
     context = {
-        "job_templates": job_templates
+        "job_templates": job_templates,
+        'breadcrumbs': breadcrumbs
     }
-    return render(request, "service_catalog/settings/tower/job_templates/job-templates-list.html", context)
+    return render(request,
+                  "service_catalog/admin/tower/job_templates/job-templates-list.html", context)
 
 
 @permission_required('service_catalog.delete_towerserver')
 def delete_tower(request, tower_id):
-    obj = get_object_or_404(TowerServer, id=tower_id)
+    tower_server = get_object_or_404(TowerServer, id=tower_id)
     if request.method == "POST":
-        obj.delete()
+        tower_server.delete()
         return redirect('service_catalog:list_tower')
+    breadcrumbs = [
+        {'text': 'Tower/AWX', 'url': reverse('service_catalog:list_tower')},
+        {'text': tower_server.name, 'url': ""}
+    ]
     context = {
-        "object": obj
+        "tower_server": tower_server,
+        'breadcrumbs': breadcrumbs
     }
-    return render(request, "service_catalog/settings/tower/tower-delete.html", context)
+    return render(request, "service_catalog/admin/tower/tower-delete.html", context)
 
 
 @permission_required('service_catalog.delete_jobtemplate')
 def delete_job_template(request, tower_id, job_template_id):
-    obj = get_object_or_404(JobTemplate, id=job_template_id)
-    obj.delete()
+    server = get_object_or_404(JobTemplate, id=job_template_id)
+    server.delete()
     return redirect('service_catalog:tower_job_templates_list', tower_id=tower_id)
 
 
@@ -80,4 +98,9 @@ def update_tower(request, tower_id):
     if form.is_valid():
         form.save()
         return redirect('service_catalog:list_tower')
-    return render(request, 'service_catalog/settings/tower/tower-edit.html', {'form': form, 'tower_server': tower_server})
+    breadcrumbs = [
+        {'text': 'Tower/AWX', 'url': reverse('service_catalog:list_tower')},
+        {'text': tower_server.name, 'url': ""}
+    ]
+    context = {'form': form, 'tower_server': tower_server, 'breadcrumbs': breadcrumbs}
+    return render(request, 'service_catalog/admin/tower/tower-edit.html', context)
