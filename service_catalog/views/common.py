0267300051db928f9da5a93c9df3c29b0adcbc44
Nicolas Marcq <nico.marcq@gmail.com>
2021-08-05 13:58:55 +0200

    add doc management

--- service_catalog/views/common.py
+++ service_catalog/views/common.py
@@ -1,9 +1,21 @@
+import json
+import os
+import uuid
+
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.core.exceptions import PermissionDenied
-from django.shortcuts import render, get_object_or_404, redirect
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
 from django_fsm import can_proceed
 from guardian.shortcuts import get_objects_for_user
+from martor.utils import LazyEncoder
 
 from profiles.models import BillingGroup
 from resource_tracker.models import ResourcePool
@@ -15,6 +27,7 @@ from service_catalog.models.request import RequestState
 from .color import map_dict_request_state, random_color, map_class_to_color
 from ..filters.instance_filter import InstanceFilter
 from ..filters.request_filter import RequestFilter
+from service_catalog.models import Doc
 
 
 def get_color_from_string(string):
@@ -283,3 +296,70 @@ def instance_list(request):
     else:
         instances = get_objects_for_user(request.user, 'service_catalog.view_instance')
         return render(request, 'service_catalog/customer/instance/instance-list.html', {'instances': instances})
+
+
+@login_required
+def markdown_uploader(request):
+    """
+    Makdown image upload for locale storage
+    and represent as json to markdown editor.
+    """
+    if request.method == 'POST' and request.is_ajax():
+        if 'markdown-image-upload' in request.FILES:
+            image = request.FILES['markdown-image-upload']
+            image_types = [
+                'image/png', 'image/jpg',
+                'image/jpeg', 'image/pjpeg', 'image/gif'
+            ]
+            if image.content_type not in image_types:
+                data = json.dumps({
+                    'status': 405,
+                    'error': _('Bad image format.')
+                }, cls=LazyEncoder)
+                return HttpResponse(
+                    data, content_type='application/json', status=405)
+
+            if image.size > settings.MAX_IMAGE_UPLOAD_SIZE:
+                to_mb = settings.MAX_IMAGE_UPLOAD_SIZE / (1024 * 1024)
+                data = json.dumps({
+                    'status': 405,
+                    'error': _('Maximum image file is %(size) MB.') % {'size': to_mb}
+                }, cls=LazyEncoder)
+                return HttpResponse(
+                    data, content_type='application/json', status=405)
+
+            img_uuid = "{0}-{1}".format(uuid.uuid4().hex[:10], image.name.replace(' ', '-'))
+            tmp_file = os.path.join(settings.MARTOR_UPLOAD_PATH, img_uuid)
+            def_path = default_storage.save(tmp_file, ContentFile(image.read()))
+            img_url = os.path.join(settings.MEDIA_URL, def_path)
+
+            data = json.dumps({
+                'status': 200,
+                'link': img_url,
+                'name': image.name
+            })
+            return HttpResponse(data, content_type='application/json')
+        return HttpResponse(_('Invalid request!'))
+    return HttpResponse(_('Invalid request!'))
+
+
+@login_required
+def doc_show(request, doc_id):
+    doc = get_object_or_404(Doc, id=doc_id)
+    breadcrumbs = [
+        {'text': 'Documentations', 'url': reverse('service_catalog:doc_list')},
+        {'text': doc.title, 'url': ""}
+    ]
+    context = {
+        "doc": doc,
+        "breadcrumbs": breadcrumbs
+    }
+    return render(request,
+                  'service_catalog/common/documentation/doc-show.html', context)
+
+
+@login_required
+def doc_list(request):
+    docs = Doc.objects.all()
+    context = {'docs': docs}
+    return render(request, 'service_catalog/common/documentation/doc-list.html', context)
