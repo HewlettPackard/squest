38fe79f8ce8d5d4c0c2470a4e1d5796f655df933
eliasboulharts <boulharts.elias@gmail.com>
2021-08-09 16:53:03 +0200

    tests for forms clean in announcements

---
+++ tests/test_forms/test_announcement_form.py
@@ -0,0 +1,45 @@
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+from service_catalog.forms import AnnouncementForm
+from service_catalog.models import AnnouncementType
+from tests.base import BaseTest
+
+
+class TestAnnouncementForm(BaseTest):
+
+    def setUp(self):
+        super(TestAnnouncementForm, self).setUp()
+
+    def test_create_announcement(self):
+        data = {
+            'title': 'My announcement title info',
+            'message': 'My announcement message info',
+            'date_start': timezone.now(),
+            'date_stop': timezone.now() + timezone.timedelta(days=2),
+            'type': AnnouncementType.INFO
+        }
+        form = AnnouncementForm(data)
+        self.assertTrue(form.is_valid)
+
+    def test_create_announcement_with_stop_before_start(self):
+        data = {
+            'title': 'My announcement title info',
+            'message': 'My announcement message info',
+            'date_start': timezone.now() + timezone.timedelta(days=2),
+            'date_stop': timezone.now(),
+            'type': AnnouncementType.INFO
+        }
+        form = AnnouncementForm(data)
+        self.assertFalse(form.is_valid())
+
+    def test_create_announcement_with_start_in_the_past(self):
+        data = {
+            'title': 'My announcement title info',
+            'message': 'My announcement message info',
+            'date_start': timezone.now() - timezone.timedelta(days=3),
+            'date_stop': timezone.now() + timezone.timedelta(days=2),
+            'type': AnnouncementType.INFO
+        }
+        form = AnnouncementForm(data)
+        self.assertFalse(form.is_valid())
