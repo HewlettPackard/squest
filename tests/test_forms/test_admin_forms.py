43c5930ab5631eec45e02e53e92a5c93d252c66d
Anthony HPE <abelhadj@hpe.com>
2021-08-10 22:46:54 +0200

    test

--- tests/test_forms/test_admin_forms.py
+++ tests/test_forms/test_admin_forms.py
@@ -1,4 +1,7 @@
+from guardian.mixins import UserObjectPermission
+
 from service_catalog.forms import AcceptRequestForm
+from service_catalog.models import Request, Instance
 from tests.base import BaseTest
 
 
@@ -6,6 +9,24 @@ class TestServiceRequestForm(BaseTest):
 
     def setUp(self):
         super(TestServiceRequestForm, self).setUp()
+        data = {
+            'text_variable': 'my_var',
+            'multiplechoice_variable': 'choice1',
+            'multiplechoice_variable': 'choice1', 'multiselect_var': 'multiselect_1',
+            'textarea_var': '2',
+            'password_var': 'pass',
+            'integer_var': '1',
+            'float_var': '0.6'
+        }
+        self.test_instance = Instance.objects.create(name="test_instance_1", service=self.service_test)
+        # give user perm on this instance
+        UserObjectPermission.objects.assign_perm('change_instance', self.standard_user, obj=self.test_instance)
+        UserObjectPermission.objects.assign_perm('view_instance', self.standard_user, obj=self.test_instance)
+        # add a first request
+        self.test_request = Request.objects.create(fill_in_survey=data,
+                                                   instance=self.test_instance,
+                                                   operation=self.create_operation_test,
+                                                   user=self.standard_user)
 
     def test_get_field_group(self):
         field_name = 'text_variable'
@@ -24,3 +45,17 @@ class TestServiceRequestForm(BaseTest):
         field_name = 'multiplechoice_variable'
         expected_result = "Admin"
         self.assertEquals(expected_result, AcceptRequestForm._get_field_group(field_name, enabled_field))
+
+    def test_accept_forms_field_count(self):
+        parameters = {
+            'request_id': self.test_request.id
+        }
+        data = {'multiselect_var': ['multiselect_2', 'multiselect_3'],
+                'text_var': ['text_val'],
+                'textarea_var': ['text_area'],
+                'password_var': ['aa'],
+                'choice_var': ['choice_2'],
+                'integer_var': ['1'],
+                'float_var': ['1.5']}
+        form = AcceptRequestForm(self.superuser, data, **parameters)
+        self.assertEquals(len(self.test_request.fill_in_survey), len(form.fields))
