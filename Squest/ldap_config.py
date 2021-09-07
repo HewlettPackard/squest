fa754518b62842b231bbfaa0d81760b094f2f5ec
Nicolas Marcq <nico.marcq@gmail.com>
2021-09-07 18:41:25 +0200

    update compose to support ldap

--- Squest/ldap_config.py
+++ Squest/ldap_config.py
@@ -10,7 +10,7 @@ AUTH_LDAP_SERVER_URI = "ldap:port"
 AUTH_LDAP_BIND_DN = "cn=service_account_name,ou=Applications,o=domain.com"
 AUTH_LDAP_BIND_PASSWORD = os.environ.get('AUTH_LDAP_BIND_PASSWORD', None)
 AUTH_LDAP_USER_SEARCH = LDAPSearch("ou=People,o=domain.com", ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
-LDAP_CA_FILE_PATH = "/path/to/ca.crt"
+LDAP_CA_FILE_PATH = "/usr/local/share/ca-certificates/ldap_ca.crt"
 AUTH_LDAP_CONNECTION_OPTIONS: {
     ldap.OPT_X_TLS_CACERTFILE: LDAP_CA_FILE_PATH,
     ldap.OPT_X_TLS_REQUIRE_CERT: ldap.OPT_X_TLS_NEVER,
@@ -21,5 +21,3 @@ AUTH_LDAP_USER_ATTR_MAP = {
     "last_name": "sn",
     "email": "uid"
 }
-
-
