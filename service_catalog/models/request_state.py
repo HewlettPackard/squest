93e69652d8a7b4d5aacb57474a01db8b543021ea
eliasboulharts <boulharts.elias@gmail.com>
2021-09-20 17:42:56 +0200

    Renamed accept button + allow to review after first accept

---
+++ service_catalog/models/request_state.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class RequestState(models.TextChoices):
+    SUBMITTED = 'SUBMITTED', _('SUBMITTED')
+    NEED_INFO = 'NEED_INFO', _('NEED_INFO')
+    REJECTED = 'REJECTED', _('REJECTED')
+    ACCEPTED = 'ACCEPTED', _('ACCEPTED')
+    CANCELED = 'CANCELED', _('CANCELED')
+    PROCESSING = 'PROCESSING', _('PROCESSING')
+    COMPLETE = 'COMPLETE', _('COMPLETE')
+    FAILED = 'FAILED', _('FAILED')
