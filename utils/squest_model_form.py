e063dd237957cdfe8db93b815c5e0d7a80d094f1
eliasboulharts <boulharts.elias@gmail.com>
2021-09-28 16:39:24 +0200

    Instance edit update

---
+++ utils/squest_model_form.py
@@ -0,0 +1,21 @@
+
+from django.forms import ModelForm, Select, CheckboxInput, CheckboxSelectMultiple, RadioSelect, FileInput
+
+
+class SquestModelForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(SquestModelForm, self).__init__(*args, **kwargs)
+        for field_name, current_field in self.fields.items():
+            if isinstance(current_field.widget, Select):
+                current_field.widget.attrs['class'] = 'form-control selectpicker'
+            elif isinstance(current_field.widget, CheckboxInput):
+                current_field.widget.attrs['class'] = ''
+            elif isinstance(current_field.widget, CheckboxSelectMultiple):
+                current_field.widget.attrs['class'] = 'form-control form-control-checkbox disable_list_style'
+            elif isinstance(current_field.widget, RadioSelect):
+                current_field.widget.attrs['class'] = "disable_list_style"
+            elif isinstance(current_field.widget, FileInput):
+                current_field.widget.attrs['class'] = ""
+            else:
+                current_field.widget.attrs['class'] = 'form-control'
