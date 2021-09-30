915cf6aaf3e2d269ae574074e0b49f2c780d9d28
Nicolas Marcq <nico.marcq@gmail.com>
2021-09-30 09:29:29 +0200

    add create operation on instance model from API
    #200

--- service_catalog/serializers/instance_serializer.py
+++ service_catalog/serializers/instance_serializer.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 
-from service_catalog.models import Instance
+from service_catalog.models import Instance, InstanceState
 
 
 class InstanceSerializer(serializers.ModelSerializer):
+    state = serializers.ChoiceField(choices=InstanceState.choices)
 
     class Meta:
         model = Instance
         fields = '__all__'
-        read_only_fields = ['service', 'state']
