9540fd81b98789a7efaeee9d00bf40e91d23ee9e
Nicolas Marcq <nico.marcq@gmail.com>
2021-10-15 15:59:29 +0200

    add git sha to the version tag in the footer

--- service_catalog/templatetags/version.py
+++ service_catalog/templatetags/version.py
@@ -8,5 +8,9 @@ def app_version():
     """
     Return Squest version as listed in `__version__` in `init.py` of settings package
     """
+    import git
     from Squest.version import __version__
-    return __version__
+    repo = git.Repo(search_parent_directories=True)
+    sha = str(repo.head.object.hexsha)[0:6]
+    version = f"{__version__} - {sha}"
+    return version
