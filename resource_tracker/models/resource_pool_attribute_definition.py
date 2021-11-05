76113c9d9739a3d958e016added8ab1193399c32
Nicolas Marcq <nico.marcq@gmail.com>
2021-11-05 18:02:52 +0100

    [Enhancement] Graph perf enhancement
    Place consumption and sum of resource attribute in the database so we do not calculate them on each view call

--- resource_tracker/models/resource_pool_attribute_definition.py
+++ resource_tracker/models/resource_pool_attribute_definition.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.db.models.signals import post_save
 from resource_tracker.models import ResourceGroupAttributeDefinition, ResourceAttribute
 
 
@@ -17,19 +17,33 @@ class ResourcePoolAttributeDefinition(models.Model):
                                       null=True)
     over_commitment_producers = models.FloatField(default=1)
     over_commitment_consumers = models.FloatField(default=1)
+    total_consumed = models.IntegerField(default=0)
+    total_produced = models.IntegerField(default=0)
 
     def __str__(self):
         return f"{self.resource_pool.name} - {self.name}"
 
     def add_producers(self, resource: ResourceGroupAttributeDefinition):
+        previous_producer = None
+        if resource.produce_for is not None:
+            previous_producer = resource.produce_for
         resource.produce_for = self
         resource.save()
+        self.calculate_total_produced()
+        if previous_producer is not None:
+            previous_producer.calculate_total_produced()
 
     def add_consumers(self, resource: ResourceGroupAttributeDefinition):
+        previous_consumer = None
+        if resource.consume_from is not None:
+            previous_consumer = resource.consume_from
         resource.consume_from = self
         resource.save()
+        self.calculate_total_consumed()
+        if previous_consumer is not None:
+            previous_consumer.calculate_total_consumed()
 
-    def get_total_produced(self):
+    def calculate_total_produced(self):
         total_produced = 0
         for producer in self.producers.all():  # producer == ResourceGroupAttributeDefinition
             # For all ResourceGroup that produce for my attribute
@@ -39,9 +53,10 @@ class ResourcePoolAttributeDefinition(models.Model):
                     total_produced += resource.attributes.get(attribute_type=producer).value
                 except ResourceAttribute.DoesNotExist:
                     pass
-        return total_produced * self.over_commitment_producers
+        self.total_produced = total_produced * self.over_commitment_producers
+        self.save()
 
-    def get_total_consumed(self):
+    def calculate_total_consumed(self):
         total_consumed = 0
         for consumer in self.consumers.all():  # consumer == ResourceGroupAttributeDefinition
             for resource in consumer.resource_group.resources.all():
@@ -49,19 +64,20 @@ class ResourcePoolAttributeDefinition(models.Model):
                     total_consumed += resource.attributes.get(attribute_type=consumer).value
                 except ResourceAttribute.DoesNotExist:
                     pass
-        return total_consumed * self.over_commitment_consumers
+        self.total_consumed = total_consumed * self.over_commitment_consumers
+        self.save()
 
     def get_percent_available_human_readable(self):
-        if self.get_total_produced() == 0:
+        if self.total_produced == 0:
             return ""
         return f"({round(100 - self.get_percent_consumed())}%)"
 
     def get_percent_consumed(self):
-        if self.get_total_produced() == 0:
+        if self.total_produced == 0:
             return "N/A"
         percent_consumed = 0
         try:
-            percent_consumed = (self.get_total_consumed() * 100) / self.get_total_produced()
+            percent_consumed = (self.total_consumed * 100) / self.total_produced
         except ZeroDivisionError:
             pass
         return round(percent_consumed)
@@ -93,3 +109,14 @@ class ResourcePoolAttributeDefinition(models.Model):
         for consumer in self.consumers.all():
             consumer.consume_from = None
             consumer.save()
+
+
+def on_change(sender, instance, created, **kwargs):
+    post_save.disconnect(on_change, sender=sender)  # prevent post save recursion
+    instance.calculate_total_consumed()
+    instance.calculate_total_produced()
+    instance.save()
+    post_save.connect(on_change, sender=sender)
+
+
+post_save.connect(on_change, sender=ResourcePoolAttributeDefinition)
