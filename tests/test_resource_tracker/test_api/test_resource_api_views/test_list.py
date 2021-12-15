4e3a59a16cc14cf4b3dee943fffe5a31e7fc489b
Nicolas Marcq <nico.marcq@gmail.com>
2021-12-15 15:37:40 +0100

    [Feature] API - get resource by name

--- tests/test_resource_tracker/test_api/test_resource_api_views/test_list.py
+++ tests/test_resource_tracker/test_api/test_resource_api_views/test_list.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.reverse import reverse
 
+from resource_tracker.api.serializers.resource_group.resource_serializers import ResourceSerializer
 from tests.test_resource_tracker.test_api.base_test_api import BaseTestAPI
 
 
@@ -20,3 +21,19 @@ class TestResourceList(BaseTestAPI):
             self.assertTrue("service_catalog_instance" in resource)
             self.assertTrue("attributes" in resource)
             self.assertTrue("text_attributes" in resource)
+
+    def test_resource_list_filter_by_name(self):
+        # test existing name
+        testing_resource = self.rg_physical_servers.create_resource(name=f"testing")
+        url = reverse('api_resource_list_create',  args=[self.rg_physical_servers.id]) + f"?name={testing_resource.name}"
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(1, len(response.data))
+        serializer = ResourceSerializer(testing_resource)
+        self.assertEqual(response.data,  [serializer.data])
+
+        # test non existing name
+        url = reverse('api_resource_list_create',  args=[self.rg_physical_servers.id]) + f"?name=do_not_exist"
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(0, len(response.data))
