fde95b15be73088637b6051ccc6a7b124c12f885
Nicolas Marcq <nico.marcq@gmail.com>
2021-12-23 14:37:06 +0100

    [Fix] fix generic list of resource after deleting an attribute
    #300

--- resource_tracker/tables/resource_tables.py
+++ resource_tracker/tables/resource_tables.py
@@ -1,4 +1,4 @@
-from django_tables2 import CheckBoxColumn, TemplateColumn
+from django_tables2 import CheckBoxColumn, TemplateColumn, Column
 from django_tables2.columns import BoundColumns
 
 from resource_tracker.models import Resource
@@ -9,12 +9,11 @@ class ResourceTable(SquestTable):
     class Meta:
         model = Resource
         attrs = {"id": "resource_table", "class": "table squest-pagination-tables"}
-        fields = ("selection", "name", "operations")
-    selection = CheckBoxColumn(accessor='pk', attrs={"th__input": {"onclick": "toggle(this)"}})
-    operations = TemplateColumn(template_name='custom_columns/resource_operations.html', orderable=False)
 
     def __init__(self, *args, **kwargs):
         super(ResourceTable, self).__init__(*args, **kwargs)
+        self.base_columns.clear()
+        self.columns = BoundColumns(self, type(self).base_columns)
         if self.data.data.count() != 0:
             resource_group = self.data.data.first().resource_group
             attribute_names = list()
@@ -29,7 +28,8 @@ class ResourceTable(SquestTable):
             sequence = [*["selection", "name"], *attribute_names, *text_attribute_names, *["operations"]]
             self.Meta.fields = tuple(sequence)
             self.sequence = sequence
-
+            type(self).base_columns["selection"] = CheckBoxColumn(accessor='pk', attrs={"th__input": {"onclick": "toggle(this)"}})
+            type(self).base_columns["name"] = Column()
             for attribute_name in attribute_names:
                 type(self).base_columns[attribute_name] = TemplateColumn(
                     template_name='custom_columns/attribute_value.html',
@@ -42,5 +42,5 @@ class ResourceTable(SquestTable):
                     extra_context={'attribute_name': attribute_name},
                     orderable=False
                 )
-            type(self).base_columns.move_to_end("operations")
+            type(self).base_columns["operations"] = TemplateColumn(template_name='custom_columns/resource_operations.html', orderable=False)
             self.columns = BoundColumns(self, type(self).base_columns)
