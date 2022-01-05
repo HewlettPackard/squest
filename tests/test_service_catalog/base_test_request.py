1822ca867dc258ebbacc266aa520e664c617b4ae
Nicolas Marcq <nico.marcq@gmail.com>
2022-01-05 14:57:53 +0100

    [Enhancement] Execute tests in memory

--- tests/test_service_catalog/base_test_request.py
+++ tests/test_service_catalog/base_test_request.py
@@ -8,7 +8,8 @@ class BaseTestRequest(BaseTest):
     def setUp(self):
         super(BaseTestRequest, self).setUp()
         form_data = {'instance_name': 'test instance', 'text_variable': 'my_var'}
-        self.test_instance = Instance.objects.create(name="test_instance_1", service=self.service_test,
+        self.test_instance = Instance.objects.create(name="test_instance_1",
+                                                     service=self.service_test,
                                                      spoc=self.standard_user)
 
         # add a first request
@@ -20,7 +21,8 @@ class BaseTestRequest(BaseTest):
         self.support_test = Support.objects.create(title="support_1", instance=self.test_instance)
 
         # second instance for second user
-        self.test_instance_2 = Instance.objects.create(name="test_instance_2", service=self.service_test,
+        self.test_instance_2 = Instance.objects.create(name="test_instance_2",
+                                                       service=self.service_test,
                                                        spoc=self.standard_user_2)
 
         self.rg_physical_servers = ResourceGroup.objects.create(name="Physical servers")
