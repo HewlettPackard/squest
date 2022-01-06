e7930b40ca01ac5e4fc9bd7b7bbbe66a5bac3d78
Nicolas Marcq <nico.marcq@gmail.com>
2022-01-06 16:38:24 +0100

    [Enhancement] use billing group serializer in instance serializer

--- service_catalog/serializers/instance_serializer.py
+++ service_catalog/serializers/instance_serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from profiles.api.serializers.billing_group_serializers import BillingGroupSerializer
 from resource_tracker.api.serializers.resource_group.resource_serializers import ResourceSerializer
 from service_catalog.models import Instance, InstanceState
 
@@ -7,6 +8,7 @@ from service_catalog.models import Instance, InstanceState
 class InstanceSerializer(serializers.ModelSerializer):
     state = serializers.ChoiceField(choices=InstanceState.choices)
     resources = ResourceSerializer(many=True, read_only=True)
+    billing_group = BillingGroupSerializer(read_only=True)
 
     class Meta:
         model = Instance
