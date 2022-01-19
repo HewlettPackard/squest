50381187a5cc23c1a56bc113b38682d6ef604d31
eliasboulharts <boulharts.elias@gmail.com>
2022-01-19 10:02:55 +0100

    [Enhancement] Update service subscription form

---
+++ docs/manual/notifications.md
@@ -0,0 +1,16 @@
+# Notifications
+
+Squest current notification system only support emails.
+
+## Enable or disable notifications
+
+By default, notifications are enabled. You can disable all notifications from your profile page by accessing the user details page in the top right corner of the Squest application.
+
+>**Note:** Administrators will not receive any notification without subscribing to some services. See below.
+
+## Subscribe to service notifications (admin)
+
+By default, administrator are not linked to any service. 
+By so, they don't receive any email notification of new requests, supports or comments.
+
+As an admin, you need to select the services you want to follow in the notification panel configuration of your user profile page.
--- profiles/forms/notification_forms.py
+++ profiles/forms/notification_forms.py
@@ -1,25 +1,28 @@
 from django.core.exceptions import ValidationError
-from django.forms import Form, ModelChoiceField, Select
+from django.forms import Form, ModelChoiceField, SelectMultiple
+from django_filters.fields import ModelMultipleChoiceField
 
 from service_catalog.models import Service
 
 
 class NotificationServiceForm(Form):
-    service = ModelChoiceField(queryset=Service.objects.all(),
-                               widget=Select(attrs={'class': 'form-control'}))
+    service = ModelMultipleChoiceField(queryset=Service.objects.all(),
+                                       widget=SelectMultiple(attrs={'class': 'form-control selectpicker',
+                                                                    'data-live-search': 'true'}))
 
     def __init__(self, user, *args, **kwargs):
         # get arguments from instance
         self.user = user
         super(NotificationServiceForm, self).__init__(*args, **kwargs)
-
-    def clean_service(self):
-        service = self.cleaned_data['service']
-        if service in self.user.profile.subscribed_services_notification.all():
-            raise ValidationError('You have already subscribed to this service')
-        return service
+        self.fields['service'].initial = [service.id for service in self.user.profile.subscribed_services_notification.all()]
 
     def save(self):
-        service = self.cleaned_data["service"]
-        self.user.profile.subscribed_services_notification.add(service)
+        service_list = self.cleaned_data.get('service')
+        current_service = [service.id for service in self.user.profile.subscribed_services_notification.all()]
+        to_remove = list(set(current_service) - set(service_list))
+        to_add = list(set(service_list) - set(current_service))
+        for service in to_remove:
+            self.user.profile.subscribed_services_notification.remove(service)
+        for service in to_add:
+            self.user.profile.subscribed_services_notification.add(service)
         self.user.save()
