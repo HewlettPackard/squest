95ba9b2d2714a45393f8809bcb325f8366c8d0dd
eliasboulharts <boulharts.elias@gmail.com>
2022-01-24 16:00:29 +0100

    [Internal] datatables.js not used anymore

---
+++ resource_tracker/tables/resource_group_attribute_definition_table.py
@@ -0,0 +1,19 @@
+from django_tables2 import TemplateColumn
+
+from resource_tracker.models import ResourceGroupAttributeDefinition
+from Squest.utils.squest_table import SquestTable
+
+
+class ResourceGroupAttributeDefinitionTable(SquestTable):
+    class Meta:
+        model = ResourceGroupAttributeDefinition
+        attrs = {"id": "resource_group_table", "class": "table squest-pagination-tables"}
+        fields = ("name", "consume_from", "produce_for", "operation")
+
+    operations = TemplateColumn(template_name='custom_columns/generic_actions_with_parent.html',
+                                orderable=False,
+                                extra_context={
+                                    'object_name': 'resource_group_attribute',
+                                    'app_name': 'resource_tracker'
+                                }
+                                )
