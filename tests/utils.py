b4f6d2f870b9d7e325d69a3a5644c44975b00879
eliasboulharts <boulharts.elias@gmail.com>
2022-02-09 11:04:23 +0100

    [Enhancement] Bulk delete resource : calculations are executed only once

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,3 @@
-from django.db.models.signals import post_save
-
-
 def check_data_in_dict(self, expected_data_list, data_list):
     for expected_data, data in zip(expected_data_list, data_list):
         for key_var, val_var in expected_data.items():
@@ -9,14 +6,3 @@ def check_data_in_dict(self, expected_data_list, data_list):
                 self.assertEqual(set(val_var), set(data[key_var]))
             else:
                 self.assertEqual(val_var, data[key_var])
-
-
-def skip_auto_calculation(func):
-    def _skip_auto_calculation(self):
-        from resource_tracker.models import ResourceAttribute
-        from resource_tracker.models.resource_attribute import on_change
-        post_save.disconnect(on_change, sender=ResourceAttribute)
-        func(self)
-        post_save.connect(on_change, sender=ResourceAttribute)
-
-    return _skip_auto_calculation
