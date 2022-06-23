7a3c91130e0f830fea472cc064ad67e61e069e42
Nicolas Marcq <nico.marcq@gmail.com>
2022-06-23 18:54:48 +0200

    [Enhancement] Add pending request for each service in the admin dashboard

--- Squest/views.py
+++ Squest/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.db.models import Count, QuerySet, F
 from django.utils import timezone
 from django.shortcuts import render
 from guardian.shortcuts import get_objects_for_user
 from service_catalog.models.announcement import Announcement
 from service_catalog.models.instance import InstanceState
 from service_catalog.models.request import RequestState
-from service_catalog.models import Request, Instance, Support
+from service_catalog.models import Request, Instance, Support, Service
+from service_catalog.models.support import SupportState
 
 
 @login_required
@@ -20,6 +22,46 @@ def home(request):
         context['total_support_opened'] = Support.objects.filter(state='OPENED').count()
         context['total_user_without_billing_groups'] = User.objects.filter(billing_groups=None).count()
         context['total_user'] = User.objects.all().count()
+
+        # Create a dict that represent {service, number_of_instance, submitted_request}
+        all_services = Service.objects.all()
+        instances = Instance.objects.filter(state=InstanceState.AVAILABLE).values('service')\
+            .annotate(service_name=F('service__name'))\
+            .annotate(instance_count=Count('service_name'))\
+            .order_by('service_name')
+        submitted_requests = Request.objects.filter(state=RequestState.SUBMITTED).values('instance__service__name')\
+            .annotate(service_name=F('instance__service__name'))\
+            .annotate(request_count=Count('service_name'))\
+            .order_by('service_name')
+        failed_requests = Request.objects.filter(state=RequestState.FAILED).values('instance__service__name') \
+            .annotate(service_name=F('instance__service__name')) \
+            .annotate(request_count=Count('service_name')) \
+            .order_by('service_name')
+        opened_supports = Support.objects.filter(state=SupportState.OPENED).values('instance__service__name') \
+            .annotate(service_name=F('instance__service__name')) \
+            .annotate(request_count=Count('service_name')) \
+            .order_by('service_name')
+
+        service_details = dict()
+        for service in all_services:
+            service_details[service.name] = {
+                "service": service,
+                "instances": 0,
+                "submitted_request": 0,
+                "failed_requests": 0,
+                "opened_supports": 0,
+            }
+            if instances.filter(service=service.id).exists():
+                service_details[service.name]["instances"] = instances.get(service=service.id)["instance_count"]
+            if submitted_requests.filter(instance__service=service.id).exists():
+                service_details[service.name]["submitted_request"] = submitted_requests.get(instance__service=service.id)["request_count"]
+            if failed_requests.filter(instance__service=service.id).exists():
+                service_details[service.name]["failed_requests"] = failed_requests.get(instance__service=service.id)["request_count"]
+            if opened_supports.filter(instance__service=service.id).exists():
+                service_details[service.name]["opened_supports"] = opened_supports.get(instance__service=service.id)["request_count"]
+
+        context["service_details"] = service_details
+
     else:
         context['total_request'] = get_objects_for_user(request.user, 'service_catalog.view_request').filter(
                 state=RequestState.SUBMITTED).count()
--- service_catalog/filters/request_filter.py
+++ service_catalog/filters/request_filter.py
@@ -9,11 +9,22 @@ from Squest.utils.squest_filter import SquestFilter
 class RequestFilter(SquestFilter):
     class Meta:
         model = Request
-        fields = ['instance__name', 'instance__id', 'user__username', 'instance__service__name', 'operation__name', 'operation__type',
+        fields = ['instance__name', 'instance__id', 'user__username', 'instance__service__id', 'operation', 'operation__type',
                   'state']
 
+    instance__service__id = MultipleChoiceFilter(
+        label="Service",
+        choices=[],
+        widget=SelectMultiple(attrs={'data-live-search': "true"}))
+
     operation__type = MultipleChoiceFilter(
+        label="Type",
         choices=OperationType.choices,
+        widget=SelectMultiple(attrs={'data-live-search': "true"}))
+
+    operation = MultipleChoiceFilter(
+        label="Operation",
+        choices=[],
         widget=SelectMultiple(attrs={'data-live-search': "true"}))
 
     state = MultipleChoiceFilter(
@@ -25,9 +36,10 @@ class RequestFilter(SquestFilter):
         self.filters['instance__name'].field.label = 'Instance'
         self.filters['instance__id'].field.widget = HiddenInput()
         self.filters['user__username'].field.label = 'User'
-        self.filters['instance__service__name'].field.label = 'Service name'
-        self.filters['operation__name'].field.label = 'Operation name'
-        self.filters['operation__type'].field.label = 'Type'
+        from service_catalog.models import Service
+        from service_catalog.models.operations import Operation
+        self.filters['instance__service__id'].field.choices = [(service.id, service.name) for service in Service.objects.all()]
+        self.filters['operation'].field.choices = [(operation.id, f"{operation.service.name}-{operation.name}") for operation in Operation.objects.all()]
 
     @property
     def qs(self):
