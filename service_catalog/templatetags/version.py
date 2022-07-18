c8121fe128a07523cee6adb1facd204b63b45fba
elias-boulharts <elias.boulharts@hpe.com>
2022-07-18 19:03:25 +0200

    [Fix] Display in notification filter pages + docs open current version

--- service_catalog/templatetags/version.py
+++ service_catalog/templatetags/version.py
@@ -4,9 +4,18 @@ register = template.Library()
 
 
 @register.simple_tag
-def app_version():
+def commit_sha():
+    """
+    Return Squest 6 first characters of commit SHA.
+    """
+    from django.conf import settings
+    return settings.SQUEST_COMMIT_SHA6
+
+
+@register.simple_tag
+def squest_version():
     """
     Return Squest version as listed in `__version__` in `init.py` of settings package
     """
-    from django.conf import settings
-    return settings.SQUEST_VERSION
+    from Squest.version import __version__
+    return __version__
