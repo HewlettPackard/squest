b84056fb54020ae3500ef702a9c066e99ce10574
Nicolas Marcq <nico.marcq@gmail.com>
2022-07-20 11:07:59 +0200

    [Enhancement] Change date format + operation name color

--- service_catalog/tables/request_tables.py
+++ service_catalog/tables/request_tables.py
@@ -9,8 +9,9 @@ class RequestTable(SquestTable):
     selection = CheckBoxColumn(accessor='pk', attrs={"th__input": {"onclick": "toggle(this)"}})
     id = LinkColumn("service_catalog:request_details", args=[A("id")])
     actions = TemplateColumn(template_name='custom_columns/request_actions.html', orderable=False)
+    date_submitted = TemplateColumn(template_name='custom_columns/generic_date_format.html')
     state = TemplateColumn(template_name='custom_columns/request_state.html')
-    operation__type = TemplateColumn(verbose_name="Type", template_name='custom_columns/request_operation_type.html')
+    operation__name = TemplateColumn(template_name='custom_columns/request_operation_name.html')
     instance__name = LinkColumn("service_catalog:instance_details", args=[A("instance__id")],
                                 verbose_name="Instance")
 
@@ -26,4 +27,4 @@ class RequestTable(SquestTable):
         model = Request
         attrs = {"id": "request_table", "class": "table squest-pagination-tables"}
         fields = ("selection", "id", "instance__name", "user", "date_submitted", "instance__service__name", "operation__name",
-                  "operation__type", "state", "actions")
+                  "state", "actions")
