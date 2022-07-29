3ba12b0436ebeff190b67064bccebb352ec355c5
Nicolas Marcq <nico.marcq@gmail.com>
2022-07-29 11:47:08 +0200

    [Enhancement] add doc description to portfolio and service

--- service_catalog/views/portfolio_view.py
+++ service_catalog/views/portfolio_view.py
@@ -10,17 +10,16 @@ from service_catalog.models.portfolio import Portfolio
 
 @user_passes_test(lambda u: u.is_superuser)
 def portfolio_create(request):
-    parent_portfolio = request.GET.get('parent_portfolio')
-    parent = get_object_or_404(Portfolio.objects.all(), id=parent_portfolio) if parent_portfolio else None
     if request.method == 'POST':
-        form = PortfolioForm(request.POST, request.FILES or None, parent_portfolio=parent_portfolio)
+        form = PortfolioForm(request.POST, request.FILES or None)
         if form.is_valid():
             form.save()
-            return redirect(get_portfolio_list_url(parent_portfolio))
+            return redirect("service_catalog:portfolio_list")
     else:
-        form = PortfolioForm(parent_portfolio=parent_portfolio)
-    breadcrumbs = get_portfolio_breadcrumbs(parent_portfolio)
-    breadcrumbs.append({'text': 'Create a portfolio', 'url': ''})
+        form = PortfolioForm()
+    breadcrumbs = [{'text': 'Service catalog', 'url': reverse('service_catalog:service_catalog_list')},
+                   {'text': 'Portfolios', 'url': reverse('service_catalog:portfolio_list')},
+                   {'text': "Create a portfolio'", 'url': ''}]
     context = {
         'form': form,
         'breadcrumbs': breadcrumbs,
@@ -33,13 +32,13 @@ def portfolio_create(request):
 @user_passes_test(lambda u: u.is_superuser)
 def portfolio_edit(request, portfolio_id):
     portfolio = get_object_or_404(Portfolio, id=portfolio_id)
-    parent_portfolio = portfolio.parent_portfolio.id if portfolio.parent_portfolio else None
-    form = PortfolioForm(request.POST or None, request.FILES or None, instance=portfolio, parent_portfolio=parent_portfolio)
+    form = PortfolioForm(request.POST or None, request.FILES or None, instance=portfolio)
     if form.is_valid():
         form.save()
-        return redirect(get_portfolio_list_url(parent_portfolio))
-    breadcrumbs = get_portfolio_breadcrumbs(parent_portfolio)
-    breadcrumbs.append({'text': f'Edit {portfolio.name}', 'url': ''})
+        return redirect("service_catalog:portfolio_list")
+    breadcrumbs = [{'text': 'Service catalog', 'url': reverse('service_catalog:service_catalog_list')},
+                   {'text': 'Portfolios', 'url': reverse('service_catalog:portfolio_list')},
+                   {'text': portfolio.name, 'url': ''}]
     context = {
         'form': form,
         'portfolio': portfolio,
@@ -53,12 +52,12 @@ def portfolio_edit(request, portfolio_id):
 @user_passes_test(lambda u: u.is_superuser)
 def portfolio_delete(request, portfolio_id):
     portfolio = get_object_or_404(Portfolio, id=portfolio_id)
-    parent_portfolio = portfolio.parent_portfolio.id if portfolio.parent_portfolio else None
     if request.method == "POST":
         portfolio.delete()
-        return redirect(get_portfolio_list_url(parent_portfolio))
-    breadcrumbs = get_portfolio_breadcrumbs(parent_portfolio)
-    breadcrumbs.append({'text': f'Delete {portfolio.name}', 'url': ''})
+        return redirect("service_catalog:portfolio_list")
+    breadcrumbs = [{'text': 'Service catalog', 'url': reverse('service_catalog:service_catalog_list')},
+                   {'text': 'Portfolios', 'url': reverse('service_catalog:portfolio_list')},
+                   {'text': portfolio.name, 'url': ''}]
     context = {
         'breadcrumbs': breadcrumbs,
         'confirm_text': mark_safe(f"Confirm deletion of <strong>{portfolio}</strong>?"),
