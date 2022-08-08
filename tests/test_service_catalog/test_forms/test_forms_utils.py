561ee9d008e0c6f5d574ade04bb4332a44801440
elias-boulharts <elias.boulharts@hpe.com>
2022-08-08 17:35:38 +0200

    [Fix] Support choices as list in AWX

--- tests/test_service_catalog/test_forms/test_forms_utils.py
+++ tests/test_service_catalog/test_forms/test_forms_utils.py
@@ -10,10 +10,13 @@ class TestServiceRequestForm(BaseTest):
             job_template_survey=self.testing_survey,
             operation_survey=self.create_operation_test.tower_survey_fields)
 
-    def test_get_choices_from_string(self):
+    def test_get_choices_as_tuples_list(self):
         test_string = "choice1\nchoice2\nchoice3"
-        expected_value = [("", "Select an option"), ("choice1", "choice1"), ("choice2", "choice2"), ("choice3", "choice3")]
-        self.assertEqual(expected_value, utils.get_choices_from_string(test_string))
+        expected_value = [("", "Select an option"), ("choice1", "choice1"), ("choice2", "choice2"),
+                          ("choice3", "choice3")]
+        self.assertEqual(expected_value, utils.get_choices_as_tuples_list(test_string))
+        test_list = ["choice1", "choice2", "choice3"]
+        self.assertEqual(expected_value, utils.get_choices_as_tuples_list(test_list))
 
     def test_create_form_fields(self):
         expected_result = len(self.testing_survey_with_validator["spec"])
