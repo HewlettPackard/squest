27ced729cd2ac76554cab9aef50454d167a6d659
elias-boulharts <elias.boulharts@hpe.com>
2022-08-11 16:32:13 +0200

    [Enhancement] Instance filter by multiple choice instead of text

--- service_catalog/filters/instance_filter.py
+++ service_catalog/filters/instance_filter.py
@@ -1,4 +1,5 @@
-from django.forms import SelectMultiple, HiddenInput, CheckboxInput
+from django.contrib.auth.models import User
+from django.forms import HiddenInput, CheckboxInput
 from django_filters import MultipleChoiceFilter, BooleanFilter
 from service_catalog.models import Instance, Service
 from service_catalog.models.instance import InstanceState
@@ -8,24 +9,19 @@ from Squest.utils.squest_filter import SquestFilter
 class InstanceFilter(SquestFilter):
     class Meta:
         model = Instance
-        fields = ['name', 'id', 'spoc__username', 'service__id', 'state', 'billing_group']
-
-    state = MultipleChoiceFilter(
-        choices=InstanceState.choices,
-        widget=SelectMultiple(attrs={'data-live-search': "true"}))
-
-    service__id = MultipleChoiceFilter(
-        widget=SelectMultiple(attrs={'data-live-search': "true"}))
+        fields = ['name', 'id', 'spoc', 'service', 'state', 'billing_group']
+    spoc = MultipleChoiceFilter()
+    state = MultipleChoiceFilter(choices=InstanceState.choices)
+    service = MultipleChoiceFilter()
 
     no_billing_groups = BooleanFilter(method='no_billing_group', label="No billing group", widget=CheckboxInput())
     no_spocs = BooleanFilter(method='no_spoc', label="No SPOC", widget=CheckboxInput())
 
     def __init__(self, *args, **kwargs):
         super(InstanceFilter, self).__init__(*args, **kwargs)
-        self.filters['spoc__username'].field.label = 'SPOC (Name)'
-        self.filters['service__id'].field.label = 'Type'
         self.filters['id'].field.widget = HiddenInput()
-        self.filters['service__id'].field.choices = [(service.id, service.name) for service in Service.objects.all()]
+        self.filters['service'].field.choices = [(service.id, service.name) for service in Service.objects.all()]
+        self.filters['spoc'].field.choices = [(spoc.id, spoc.username) for spoc in User.objects.all()]
 
     def no_billing_group(self, queryset, name, value):
         if not value:
