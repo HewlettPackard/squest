bad706cc4685a7a9ad8326c4d49227827133d9bf
elias-boulharts <elias.boulharts@hpe.com>
2022-08-11 16:32:13 +0200

    [Enhancement] Instance filter by multiple choice instead of text

--- Squest/utils/squest_filter.py
+++ Squest/utils/squest_filter.py
@@ -1,5 +1,5 @@
 import django_filters
-from django import forms
+from django.forms import TextInput, Select, SelectMultiple, CheckboxInput
 
 
 class SquestFilter(django_filters.FilterSet):
@@ -7,14 +7,15 @@ class SquestFilter(django_filters.FilterSet):
     def __init__(self, *args, **kwargs):
         super(SquestFilter, self).__init__(*args, **kwargs)
         for filter_name, current_filter in self.filters.items():
-            if isinstance(current_filter.field.widget, forms.TextInput):
+            if isinstance(current_filter.field.widget, TextInput):
                 current_filter.field.widget.attrs['class'] = 'form-control'
                 current_filter.lookup_expr = 'icontains'
                 if self.request and '/api/' in self.request.path:
                     current_filter.lookup_expr = 'exact'
-            elif isinstance(current_filter.field.widget, forms.Select):
+            elif isinstance(current_filter.field.widget, Select) or isinstance(current_filter.field.widget, SelectMultiple):
                 current_filter.field.widget.attrs['class'] = 'form-control selectpicker'
-            elif isinstance(current_filter.field.widget, forms.CheckboxInput):
+                current_filter.field.widget.attrs['data-live-search'] = 'true'
+            elif isinstance(current_filter.field.widget, CheckboxInput):
                 current_filter.field.widget.attrs['class'] = 'form-control-checkbox'
             else:
                 current_filter.field.widget.attrs['class'] = 'form-control'
