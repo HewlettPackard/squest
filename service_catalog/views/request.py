a4edc4ef25fbb20366e2a63aedd7d0e2e165c779
Nicolas Marcq <nico.marcq@gmail.com>
2022-09-15 16:40:08 +0200

    [Enhancement] add request messages to request details

--- service_catalog/views/request.py
+++ service_catalog/views/request.py
@@ -103,7 +103,9 @@ def request_comment_edit(request, request_id, comment_id):
 def request_details(request, request_id):
     request_list = get_objects_for_user(request.user, 'service_catalog.view_request')
     target_request = get_object_or_404(request_list, id=request_id)
+    comment_messages = RequestMessage.objects.filter(request=target_request)
     context = {'target_request': target_request,
+               'comment_messages': comment_messages,
                'approval_steps': target_request.get_approval_workflow_status(),
                'breadcrumbs': [
                    {'text': 'Requests', 'url': reverse('service_catalog:request_list')},
