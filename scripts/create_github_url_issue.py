682a15a5e004c9d3e4c8732ba6f44fc00951b008
elias-boulharts <elias.boulharts@hpe.com>
2022-10-26 11:59:10 +0200

    [Internal] Add some parameters in create github url issue script

--- scripts/create_github_url_issue.py
+++ scripts/create_github_url_issue.py
@@ -2,9 +2,11 @@ import urllib.parse as urlparse
 from urllib.parse import urlencode
 
 
-def create_url_for_github_issue(github_endpoint_issue, title, body):
+def create_url_for_github_issue(github_endpoint_issue, issue_title="", issue_body="", assignees_list="", labels_list="",
+                                projects_list=""):
     url = github_endpoint_issue
-    params = {'title': title, 'body': body}
+    params = {'title': issue_title, 'body': issue_body, 'assignees': assignees_list, 'labels': labels_list,
+              'projects': projects_list}
     url_parts = list(urlparse.urlparse(url))
     query = dict(urlparse.parse_qsl(url_parts[4]))
     query.update(params)
@@ -14,10 +16,33 @@ def create_url_for_github_issue(github_endpoint_issue, title, body):
     return url_to_open_issue
 
 
-github_endpoint_new_issue = "https://github.com/HewlettPackard/squest/issues/new?"
-title = 'Templated Github issue'
-body = '''Instance <a href="https://mysquest.net/ui/service_catalog/instance/JINJA_REPLACE/">JINJA_REPLACE</a>'''
-my_link = create_url_for_github_issue(github_endpoint_new_issue, title, body)
-print(my_link)
+if __name__ == '__main__':
+    # Replace your-repository-path by your repository
+    github_endpoint_new_issue = "https://github.com/<your-repository-path>/issues/new?"
 
-print("Replace all 'JINJA_REPLACE' with the jinja code you need. E.g '{{ instance.name }}'")
+    # example of title
+    title = '#SQUEST_INSTANCE_ID - '
+
+    # example of body
+    body = \
+'''# My Squest instance details (auto-generated do not edit)
+Squest Instance: SQUEST_INSTANCE_NAME (<a href="https://squest.gre.hpecorp.net/ui/service_catalog/instance/SQUEST_INSTANCE_ID/">#SQUEST_INSTANCE_ID</a>)
+# My issue
+
+'''
+
+    # assignee username list as string separated by comma
+    assignees = "user1,user2"
+
+    # label name list as string separated by comma
+    labels = "label1,label2"
+
+    # project path list as string separated by comma
+    projects = "project_path_1/<PROJECT_ID_1>,project_path_2/<PROJECT_ID_2>"
+
+    my_link = create_url_for_github_issue(github_endpoint_new_issue, title, body, assignees, labels, projects)
+
+    # use replace for templatizing your link as you want with jinja
+    my_link = my_link.replace('SQUEST_INSTANCE_NAME', '{{instance.name}}').replace('SQUEST_INSTANCE_ID', '{{instance.id}}')
+
+    print(my_link)
