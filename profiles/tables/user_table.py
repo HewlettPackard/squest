54c3667158c1eed21b4cb078cef0854450f5df90
Nicolas Marcq <nico.marcq@gmail.com>
2022-12-13 16:05:23 +0100

    Link user to instance list with filter on spoc

--- profiles/tables/user_table.py
+++ profiles/tables/user_table.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.models import User
-from django_tables2 import tables, TemplateColumn
+from django.urls import reverse
+from django.utils.html import format_html
+from django_tables2 import tables, TemplateColumn, A, LinkColumn
 
 
 class UserTable(tables.Table):
+    username = LinkColumn("service_catalog:instance_list")
     billing_groups = TemplateColumn(template_name='custom_columns/user_billing_groups.html', orderable=False)
+
+    def render_username(self, record):
+        url = reverse('service_catalog:instance_list')
+        return format_html(f"<a href='{url}?spoc={record.id}'>{record}</a>")
 
     class Meta:
         model = User
