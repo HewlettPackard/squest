e8975a8008db32b3ee5441725d8acd611acf06f9
Nicolas Marcq <nico.marcq@gmail.com>
2023-03-06 18:14:53 +0100

    Fix mariadb init script

--- docker/scripts/mariadb-init.sh
+++ docker/scripts/mariadb-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-mysql --user=root --password=${DB_ROOT_PASSWORD} <<-EOSQL
-    GRANT ALL PRIVILEGES ON test_squest_db.* TO '${DB_USER}'@'%';
+mysql --user=root --password=${MYSQL_ROOT_PASSWORD} <<-EOSQL
+    GRANT ALL PRIVILEGES ON test_squest_db.* TO '${MYSQL_USER}'@'%';
 EOSQL
