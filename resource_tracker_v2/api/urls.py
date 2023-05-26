5236097d530e45eaa7d6ffc3c35a32abb037ac8d
Nicolas Marcq <nico.marcq@gmail.com>
2023-05-26 12:20:27 +0200

    api attribute definition

--- resource_tracker_v2/api/urls.py
+++ resource_tracker_v2/api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
+from resource_tracker_v2.api.views.attribute_definition_api_views import AttributeDefinitionList, \
+    AttributeDefinitionDetails
 from resource_tracker_v2.api.views.resource_group_api_views import ResourceGroupList, ResourceGroupDetails
 
 urlpatterns = [
+    # attribute definition
+    path('attributes/', AttributeDefinitionList.as_view(), name='api_attribute_definition_list_create'),
+    path('attributes/<int:pk>/', AttributeDefinitionDetails.as_view(), name='api_attribute_definition_details'),
+
     # resource group
     path('resource_group/', ResourceGroupList.as_view(), name='api_resource_group_list_create'),
     path('resource_group/<int:pk>/', ResourceGroupDetails.as_view(), name='api_resource_group_details'),
