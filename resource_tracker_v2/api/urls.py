c7a60cae958ead634cbc1cb312ef283e158a4f36
Nicolas Marcq <nico.marcq@gmail.com>
2023-05-31 16:20:33 +0200

    resource api views

--- resource_tracker_v2/api/urls.py
+++ resource_tracker_v2/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from resource_tracker_v2.api.views.attribute_definition_api_views import AttributeDefinitionList, \
     AttributeDefinitionDetails
-from resource_tracker_v2.api.views.resource_api_view import ResourceListCreate
+from resource_tracker_v2.api.views.resource_api_view import ResourceListCreate, ResourceDetails
 from resource_tracker_v2.api.views.resource_group_api_views import ResourceGroupList, ResourceGroupDetails
 
 urlpatterns = [
@@ -14,6 +14,10 @@ urlpatterns = [
     path('resource_group/', ResourceGroupList.as_view(), name='api_resource_group_list_create'),
     path('resource_group/<int:pk>/', ResourceGroupDetails.as_view(), name='api_resource_group_details'),
 
+    # resource
     path('resource_group/<int:resource_group_id>/resources/', ResourceListCreate.as_view(),
          name='api_resource_list_create'),
+    path('resource_group/<int:resource_group_id>/resources/<int:pk>/', ResourceDetails.as_view(),
+         name='api_resource_details'),
+
 ]
