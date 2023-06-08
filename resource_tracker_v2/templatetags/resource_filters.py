2ae8e7ede18ab224c9f5862e17730cf0f11b7484
Nicolas Marcq <nico.marcq@gmail.com>
2023-06-08 17:31:21 +0200

    code review

--- resource_tracker_v2/templatetags/resource_filters.py
+++ resource_tracker_v2/templatetags/resource_filters.py
@@ -7,8 +7,7 @@ from resource_tracker_v2.models import ResourceAttribute, AttributeDefinition
 def v2_get_attribute_value_from_name(resource, attribute_name):
     try:
         attribute = ResourceAttribute.objects.get(resource=resource,
-                                                  attribute_definition=AttributeDefinition.objects.
-                                                  get(name=attribute_name))
+                                                  attribute_definition__name=attribute_name)
         return attribute.value
     except ResourceAttribute.DoesNotExist:
         return ""
