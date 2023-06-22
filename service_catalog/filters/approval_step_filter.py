7134a5c26c6f77b4a85f24f17331c1f016d87846
Anthony Belhadj <abelhadj@hpe.com>
2023-06-22 18:30:30 +0200

    approval

--- service_catalog/filters/approval_step_filter.py
+++
@@ -1,8 +0,0 @@
-from Squest.utils.squest_filter import SquestFilter
-from service_catalog.models.approval_step import ApprovalStep
-
-
-class ApprovalStepFilter(SquestFilter):
-    class Meta:
-        model = ApprovalStep
-        fields = ["type", "teams"]
