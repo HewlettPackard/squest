4b5be5c29bc861fa256d005202d8988d660bc265
Anthony Belhadj <abelhadj@hpe.com>
2023-06-22 18:30:30 +0200

    approval

--- service_catalog/filters/approval_workflow_filter.py
+++
@@ -1,8 +0,0 @@
-from Squest.utils.squest_filter import SquestFilter
-from service_catalog.models.approval_workflow import ApprovalWorkflow
-
-
-class ApprovalWorkflowFilter(SquestFilter):
-    class Meta:
-        model = ApprovalWorkflow
-        fields = ["name"]
