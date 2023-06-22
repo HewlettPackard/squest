c65a4d44359af073d5ee542ba4f87c8606e8c178
Anthony Belhadj <abelhadj@hpe.com>
2023-06-22 17:40:04 +0200

    Update resource tracker for generics

--- resource_tracker_v2/views/resource_group_resource_views.py
+++ resource_tracker_v2/views/resource_group_resource_views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import get_object_or_404, redirect, render
@@ -28,11 +29,11 @@ class ResourceListView(TagFilterListView):
         context = super().get_context_data(**kwargs)
         context['resource_group_id'] = resource_group_id
         context['breadcrumbs'] = [
-            {'text': 'Resource groups', 'url': reverse('resource_tracker:resource_group_list')},
+            {'text': 'Resource groups', 'url': reverse('resource_tracker_v2:resourcegroup_list')},
             {'text': resource_group.name, 'url': ""},
             {'text': "Resources", 'url': ""}
         ]
-        context['action_url'] = reverse('resource_tracker:resource_group_resource_bulk_delete_confirm',
+        context['action_url'] = reverse('resource_tracker_v2:resourcegroup_resource_bulk_delete_confirm',
                                         kwargs={'resource_group_id': resource_group_id})
         context['html_button_path'] = "resource_tracking_v2/resource_group/resources/resource_list_buttons.html"
         return context
@@ -59,9 +60,9 @@ class ResourceCreateView(PermissionRequiredMixin, CreateView):
         context["resource_group"] = resource_group
         context["action"] = "create"
         context['breadcrumbs'] = [
-            {'text': 'Resource groups', 'url': reverse('resource_tracker:resource_group_list')},
+            {'text': 'Resource groups', 'url': reverse('resource_tracker_v2:resourcegroup_list')},
             {'text': resource_group.name,
-             'url': reverse('resource_tracker:resource_group_resource_list', args=[resource_group_id])},
+             'url': reverse('resource_tracker_v2:resourcegroup_resource_list', args=[resource_group_id])},
             {'text': 'New resource', 'url': ""},
         ]
         return context
@@ -87,14 +88,17 @@ class ResourceEditView(PermissionRequiredMixin, UpdateView):
         resource_id = self.kwargs.get('resource_id')
         resource = Resource.objects.get(id=resource_id)
         context = super().get_context_data(**kwargs)
-        context['title'] = "Resource group"
-        context['app_name'] = "resource_tracker"
-        context['object_name'] = "resource_group"
+
+        content_type = ContentType.objects.get_for_model(self.model)
+
+        context['title'] = f'Edit {content_type.name}'
+        context['app_name'] = content_type.app_label
+        context['object_name'] = content_type.model
         context['action'] = "edit"
         context[""] = [
-            {'text': 'Resource groups', 'url': reverse('resource_tracker:resource_group_list')},
+            {'text': 'Resource groups', 'url': reverse('resource_tracker_v2:resourcegroup_list')},
             {'text': resource_group.name,
-             'url': reverse('resource_tracker:resource_group_resource_list', args=[resource_group_id])},
+             'url': reverse('resource_tracker_v2:resourcegroup_resource_list', args=[resource_group_id])},
             {'text': resource.name, 'url': ""},
         ]
         return context
@@ -108,7 +112,7 @@ class ResourceDeleteView(PermissionRequiredMixin, DeleteView):
 
     def get_success_url(self):
         resource_group_id = self.kwargs.get('resource_group_id')
-        return reverse("resource_tracker:resource_group_resource_list",
+        return reverse("resource_tracker_v2:resourcegroup_resource_list",
                        kwargs={"resource_group_id": resource_group_id})
 
     def get_context_data(self, **kwargs):
@@ -120,9 +124,9 @@ class ResourceDeleteView(PermissionRequiredMixin, DeleteView):
         context["resource"] = resource
         context["resource_group"] = resource_group
         context['breadcrumbs'] = [
-            {'text': 'Resource groups', 'url': reverse('resource_tracker:resource_group_list')},
+            {'text': 'Resource groups', 'url': reverse('resource_tracker_v2:resourcegroup_list')},
             {'text': resource_group.name,
-             'url': reverse('resource_tracker:resource_group_resource_list', args=[resource_group_id])},
+             'url': reverse('resource_tracker_v2:resourcegroup_resource_list', args=[resource_group_id])},
             {'text': resource.name, 'url': ""},
         ]
         return context
@@ -134,20 +138,20 @@ def resource_group_resource_bulk_delete(request, resource_group_id):
         pks = request.POST.getlist("selection")
         selected_resources = Resource.objects.filter(pk__in=pks)
         selected_resources.delete()
-    return redirect("resource_tracker:resource_group_resource_list", resource_group_id)
+    return redirect("resource_tracker_v2:resourcegroup_resource_list", resource_group_id)
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def resource_group_resource_bulk_delete_confirm(request, resource_group_id):
     context = {
         'breadcrumbs': [
-            {'text': 'Resource groups', 'url': reverse('resource_tracker:resource_group_list')},
-            {'text': 'Resources', 'url': reverse('resource_tracker:resource_group_resource_list',
+            {'text': 'Resource groups', 'url': reverse('resource_tracker_v2:resourcegroup_list')},
+            {'text': 'Resources', 'url': reverse('resource_tracker_v2:resourcegroup_resource_list',
                                                  kwargs={'resource_group_id': resource_group_id})},
             {'text': "Delete multiple", 'url': ""}
         ],
         'confirm_text': mark_safe(f"Confirm deletion of the following resources?"),
-        'action_url': reverse('resource_tracker:resource_group_resource_bulk_delete', kwargs={
+        'action_url': reverse('resource_tracker_v2:resourcegroup_resource_bulk_delete', kwargs={
             "resource_group_id": resource_group_id
         }),
         'button_text': 'Delete',
@@ -159,4 +163,4 @@ def resource_group_resource_bulk_delete_confirm(request, resource_group_id):
         if context['object_list']:
             return render(request, 'generics/confirm-bulk-delete-template.html', context=context)
     messages.warning(request, 'No resources selected for deletion.')
-    return redirect('resource_tracker:resource_group_resource_list', resource_group_id=resource_group_id)
+    return redirect('resource_tracker_v2:resourcegroup_resource_list', resource_group_id=resource_group_id)
