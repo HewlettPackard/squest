720eb97689e272e1996e42327f1c26c86ff93a83
Anthony Belhadj <abelhadj@hpe.com>
2023-06-22 17:40:04 +0200

    Update resource tracker for generics

--- resource_tracker_v2/urls.py
+++ resource_tracker_v2/urls.py
@@ -2,53 +2,53 @@ from django.urls import path
 
 from resource_tracker_v2 import views
 
-app_name = 'resource_tracker'
+app_name = 'resource_tracker_v2'
 
 urlpatterns = [
     # attributes
-    path('attributes/', views.AttributeDefinitionListView.as_view(), name='attribute_definition_list'),
-    path('attributes/create/', views.AttributeDefinitionCreateView.as_view(), name='attribute_definition_create'),
+    path('attributes/', views.AttributeDefinitionListView.as_view(), name='attributedefinition_list'),
+    path('attributes/create/', views.AttributeDefinitionCreateView.as_view(), name='attributedefinition_create'),
     path('attributes/<int:attribute_definition_id>/edit/', views.AttributeDefinitionEditView.as_view(),
-         name='attribute_definition_edit'),
+         name='attributedefinition_edit'),
     path('attributes/<int:attribute_definition_id>/delete/', views.AttributeDefinitionDeleteView.as_view(),
-         name='attribute_definition_delete'),
+         name='attributedefinition_delete'),
 
     # resource group
-    path('resource_group/', views.ResourceGroupListView.as_view(), name='resource_group_list'),
-    path('resource_group/create/', views.ResourceGroupCreateView.as_view(), name='resource_group_create'),
+    path('resource_group/', views.ResourceGroupListView.as_view(), name='resourcegroup_list'),
+    path('resource_group/create/', views.ResourceGroupCreateView.as_view(), name='resourcegroup_create'),
     path('resource_group/<int:resource_group_id>/edit/', views.ResourceGroupEditView.as_view(),
-         name='resource_group_edit'),
+         name='resourcegroup_edit'),
     path('resource_group/<int:resource_group_id>/delete/', views.ResourceGroupDeleteView.as_view(),
-         name='resource_group_delete'),
+         name='resourcegroup_delete'),
 
     # resource group attributes
     path('resource_group/<int:resource_group_id>/attributes/', views.TransformerListView.as_view(),
-         name='resource_group_attribute_list'),
+         name='resourcegroup_attribute_list'),
     path('resource_group/<int:resource_group_id>/attributes/create/',
          views.TransformerCreateView.as_view(),
-         name='resource_group_attribute_create'),
+         name='resourcegroup_attribute_create'),
     path('resource_group/<int:resource_group_id>/attributes/<int:attribute_id>/edit/',
          views.TransformerEditView.as_view(),
-         name='resource_group_attribute_edit'),
+         name='resourcegroup_attribute_edit'),
     path('resource_group/<int:resource_group_id>/attributes/<int:attribute_id>/delete/',
          views.TransformerDeleteView.as_view(),
-         name='resource_group_attribute_delete'),
+         name='resourcegroup_attribute_delete'),
     path('tool/resource_group/load-resource-group-attribute/', views.ajax_load_attribute,
          name='ajax_load_attribute'),
     path('resource_group/<int:resource_group_id>/resources/', views.ResourceListView.as_view(),
-         name='resource_group_resource_list'),
+         name='resourcegroup_resource_list'),
     path('resource_group/<int:resource_group_id>/resources/create/', views.ResourceCreateView.as_view(),
-         name='resource_group_resource_create'),
+         name='resourcegroup_resource_create'),
     path('resource_group/<int:resource_group_id>/resources/<int:resource_id>/edit/', views.ResourceEditView.as_view(),
-         name='resource_group_resource_edit'),
+         name='resourcegroup_resource_edit'),
     path('resource_group/<int:resource_group_id>/resources/<int:resource_id>/delete/', views.ResourceDeleteView.as_view(),
-         name='resource_group_resource_delete'),
+         name='resourcegroup_resource_delete'),
     path('resource_group/<int:resource_group_id>/resources/delete-confirm/',
          views.resource_group_resource_bulk_delete_confirm,
-         name='resource_group_resource_bulk_delete_confirm'),
+         name='resourcegroup_resource_bulk_delete_confirm'),
     path('resource_group/<int:resource_group_id>/resources/delete-force/',
          views.resource_group_resource_bulk_delete,
-         name='resource_group_resource_bulk_delete'),
+         name='resourcegroup_resource_bulk_delete'),
 
     # graph
     path('resource_tracker_graph/', views.resource_tracker_graph, name='resource_tracker_graph'),
