782ecdc331c50179565681acdbac0830e55b9539
Anthony Belhadj <abelhadj@hpe.com>
2023-06-22 19:09:48 +0200

    RBAC view

--- profiles/urls.py
+++ profiles/urls.py
@@ -41,7 +41,9 @@ urlpatterns = [
          name='support_notification_edit'),
 
     # Scope
-    path('scope/<int:scope_id>/role/<int:pk>/delete/', views.scope_role_delete, name="scope_role_delete"),
+
+    path('scope/<int:scope_id>/rbac/create/', views.scope_rbac_create, name="scope_rbac_create"),
+    path('scope/<int:scope_id>/rbac/<int:rbac_id>/user/<int:user_id>/delete/', views.scope_rbac_delete, name="scope_rbac_delete"),
 
     # Organization
     path('organization/', views.OrganizationListView.as_view(), name="organization_list"),
@@ -49,9 +51,7 @@ urlpatterns = [
     path('organization/<int:pk>/edit/', views.OrganizationEditView.as_view(), name="organization_edit"),
     path('organization/<int:pk>/delete/', views.OrganizationDeleteView.as_view(), name="organization_delete"),
     path('organization/<int:pk>/', views.OrganizationDetailView.as_view(), name="organization_details"),
-    path('organization/<int:pk>/role/create/', views.organization_role_create, name="organization_role_create"),
-    path('organization/<int:pk>/rbac/create/', views.organization_rbac_create, name="organization_rbac_create"),
-    path('organization/<int:scope_id>/rbac/<int:rbac_id>/user/<int:user_id>/delete/', views.organization_rbac_delete, name="organization_rbac_delete"),
+
     path('organization/<int:pk>/team/create/', views.OrganizationTeamCreateView.as_view(), name="organization_team_create"),
 
 
@@ -61,9 +61,7 @@ urlpatterns = [
     path('team/<int:pk>/edit', views.TeamEditView.as_view(), name="team_edit"),
     path('team/<int:pk>/delete', views.TeamDeleteView.as_view(), name="team_delete"),
     path('team/<int:pk>/', views.TeamDetailView.as_view(), name="team_details"),
-    path('team/<int:pk>/role/create/', views.team_role_create, name="team_role_create"),
-    path('team/<int:pk>/rbac/create/', views.team_rbac_create, name="team_rbac_create"),
-    path('team/<int:scope_id>/rbac/<int:rbac_id>/user/<int:user_id>/delete/', views.team_rbac_delete, name="team_rbac_delete"),
+
 
     # Role
     path('role/', views.RoleListView.as_view(), name="role_list"),
