ef72769e70b6287efa00de71baccaf7b7ab976ac
Anthony Belhadj <abelhadj@hpe.com>
2023-06-22 18:00:06 +0200

    templatetags to get appname/class

--- profiles/templatetags/squest_utils.py
+++ profiles/templatetags/squest_utils.py
@@ -1,6 +1,17 @@
+from django.contrib.contenttypes.models import ContentType
 from django.template.defaulttags import register
 
 
 @register.filter(name='lookup')
 def lookup(value, arg):
     return value[arg]
+
+
+@register.filter
+def to_class_name(value):
+    return value.__class__.__name__
+
+
+@register.filter
+def to_app_name(value):
+    return ContentType.objects.get_for_model(value).app_label
