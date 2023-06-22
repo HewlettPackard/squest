d16ba2a35f1f130668f559bf1c4709105feb61df
Anthony Belhadj <abelhadj@hpe.com>
2023-06-22 18:06:43 +0200

    clean

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,12 +3,9 @@ from django.urls import path
 
 from Squest import settings
 from . import views
-from .views.group_list_view import GroupListView
-from .views.team_list_view import TeamListView
-from .views.user_by_group_list_view import UserByGroupListView
-from .views.user_list_view import UserListView
 
 app_name = 'profiles'
+
 urlpatterns = [
     # common URLs
     path('profile/', views.profile, name='profile'),
@@ -17,27 +14,8 @@ urlpatterns = [
     path('profile/token/<int:token_id>/edit/', views.token_edit, name='token_edit'),
     path('profile/token/<int:token_id>/delete/', views.token_delete, name='token_delete'),
 
-    path('user/', UserListView.as_view(), name='user_list'),
-
-    # group URLs
-    path('group/<int:group_id>/users/', UserByGroupListView.as_view(), name='user_by_group_list'),
-    path('group/<int:group_id>/users/update/', views.user_in_group_update, name='user_in_group_update'),
-    path('group/<int:group_id>/users/remove/<int:user_id>/', views.user_in_group_remove, name='user_in_group_remove'),
-    path('group/', GroupListView.as_view(), name='group_list'),
-    path('group/create/', views.group_create, name='group_create'),
-    path('group/<int:group_id>/edit/', views.group_edit, name='group_edit'),
-    path('group/<int:group_id>/delete/', views.group_delete, name='group_delete'),
-
-    # billing group URLs
-    path('billing-group/<int:billing_group_id>/users/update/', views.user_in_billing_group_update,
-         name='user_in_billing_group_update'),
-    path('billing-group/<int:billing_group_id>/users/remove/<int:user_id>/',
-         views.user_in_billing_group_remove, name='user_in_billing_group_remove'),
-    path('billing-group/', views.billing_group_list, name='billing_group_list'),
-    path('billing-group/create/', views.billing_group_create, name='billing_group_create'),
-    path('billing-group/<int:billing_group_id>/edit/', views.billing_group_edit, name='billing_group_edit'),
-    path('billing-group/<int:billing_group_id>/delete/', views.billing_group_delete, name='billing_group_delete'),
-    path('billing-group/<int:billing_group_id>/refresh_quota/', views.billing_group_refresh_quota, name='billing_group_refresh_quota'),
+    path('user/', views.UserListView.as_view(), name='user_list'),
+    path('user/<int:pk>/', views.UserDetailsView.as_view(), name='user_details'),
 
     # request notifications
     path('notification/request/switch/', views.request_notification_switch, name='request_notification_switch'),
@@ -62,7 +40,6 @@ urlpatterns = [
          views.support_notification_edit,
          name='support_notification_edit'),
 
-
     # Scope
     path('scope/<int:scope_id>/role/<int:pk>/delete/', views.scope_role_delete, name="scope_role_delete"),
 
@@ -77,9 +54,6 @@ urlpatterns = [
     path('organization/<int:scope_id>/rbac/<int:rbac_id>/user/<int:user_id>/delete/', views.organization_rbac_delete, name="organization_rbac_delete"),
     path('organization/<int:pk>/team/create/', views.OrganizationTeamCreateView.as_view(), name="organization_team_create"),
 
-    # User Role Binding URLs
-    path('role/ajax/get-users-with-role/', views.ajax_get_users_with_role, name='get_users_with_role'),
-    path('role/ajax/get-teams-with-role/', views.ajax_get_teams_with_role, name='get_teams_with_role'),
 
     # Team
     path('team/', views.TeamListView.as_view(), name="team_list"),
@@ -91,5 +65,11 @@ urlpatterns = [
     path('team/<int:pk>/rbac/create/', views.team_rbac_create, name="team_rbac_create"),
     path('team/<int:scope_id>/rbac/<int:rbac_id>/user/<int:user_id>/delete/', views.team_rbac_delete, name="team_rbac_delete"),
 
+    # Role
+    path('role/', views.RoleListView.as_view(), name="role_list"),
+    path('role/create/', views.RoleCreateView.as_view(), name="role_create"),
+    path('role/<int:pk>/edit/', views.RoleEditView.as_view(), name="role_edit"),
+    path('role/<int:pk>/delete/', views.RoleDeleteView.as_view(), name="role_delete"),
+    path('role/<int:pk>/', views.RoleDetailView.as_view(), name="role_details"),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
