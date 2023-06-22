2318124457070326b076180d40a17f47317557e4
Anthony Belhadj <abelhadj@hpe.com>
2023-06-22 18:07:34 +0200

    RBAC API

---
+++ profiles/api/serializers/role_serializer.py
@@ -0,0 +1,10 @@
+from rest_framework.serializers import ModelSerializer
+
+from profiles.models import Role
+
+
+class RoleSerializer(ModelSerializer):
+
+    class Meta:
+        model = Role
+        fields = ('name', 'description', 'permissions')
