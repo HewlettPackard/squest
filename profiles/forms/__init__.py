03ea131e3eaff16fe432e8e4ab47c685f767f29f
elias-boulharts <elias.boulharts@hpe.com>
2023-06-23 17:18:04 +0200

    add editing roles and rbac in global permission

--- profiles/forms/__init__.py
+++ profiles/forms/__init__.py
@@ -3,3 +3,4 @@ from .request_notification_filter_forms import *
 from .support_notification_filter_forms import *
 from .organization_forms import *
 from .role_forms import *
+from .globalpermission_forms import *
