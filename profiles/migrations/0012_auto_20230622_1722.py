c8cd653d0adc52f51a987d7bd45ff19b8eb65371
elias-boulharts <elias.boulharts@hpe.com>
2023-06-23 16:46:35 +0200

    rename squest scope to global permission

--- profiles/migrations/0012_auto_20230622_1722.py
+++ profiles/migrations/0012_auto_20230622_1722.py
@@ -35,7 +35,7 @@ class Migration(migrations.Migration):
             bases=('profiles.abstractscope',),
         ),
         migrations.CreateModel(
-            name='SquestScope',
+            name='GlobalPermission',
             fields=[
                 ('abstractscope_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='profiles.abstractscope')),
             ],
