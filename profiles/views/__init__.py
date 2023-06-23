14df185509939d03e948f191c8babfec9fc4ab1b
elias-boulharts <elias.boulharts@hpe.com>
2023-06-23 17:49:51 +0200

    fix scope abstract

--- profiles/views/__init__.py
+++ profiles/views/__init__.py
@@ -7,3 +7,4 @@ from profiles.views.organization_views import *
 from profiles.views.scope_views import *
 from profiles.views.user_views import *
 from profiles.views.role_views import *
+from profiles.views.globalpermission_views import *
