1599602cd728fe727068b34c3236785579e0890d
Anthony Belhadj <abelhadj@hpe.com>
2023-06-29 14:12:24 +0200

    Rename app_name to be generic

--- tests/test_service_catalog/test_views/test_admin/test_tools/test_global_hooks/test_delete.py
+++ tests/test_service_catalog/test_views/test_admin/test_tools/test_global_hooks/test_delete.py
@@ -13,9 +13,9 @@ class GlobalHookDeleteViewsTest(BaseTest):
                                                           state="PROVISIONING",
                                                           job_template=self.job_template_test)
         args = {
-            "global_hook_id": self.global_hook_test.id
+            "globalhook_id": self.global_hook_test.id
         }
-        self.url = reverse('service_catalog:global_hook_delete', kwargs=args)
+        self.url = reverse('service_catalog:globalhook_delete', kwargs=args)
 
     def test_can_delete_global_hook(self):
         response = self.client.get(self.url)
