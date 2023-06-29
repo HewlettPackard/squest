f67170258d24dcee88e826c9533e3155f98b5066
Anthony Belhadj <abelhadj@hpe.com>
2023-06-29 17:00:44 +0200

    RBAC for support

--- profiles/apps.py
+++ profiles/apps.py
@@ -24,6 +24,17 @@ def create_roles(sender, **kwargs):
                 role.permissions.add(permission)
 
 
+def create_permissions(sender, **kwargs):
+    from django.contrib.auth.models import Permission
+    from django.contrib.contenttypes.models import ContentType
+    from Squest.default_permissions import permissions
+    for permission in permissions:
+        content_type = ContentType.objects.get(app_label=permission["app_label"], model=permission["model"])
+        permission = Permission.objects.get_or_create(
+            codename=permission["codename"],
+            content_type=content_type,
+            defaults={"name": permission["name"]}
+        )
 
 
 class ProfilesConfig(AppConfig):
@@ -31,3 +42,4 @@ class ProfilesConfig(AppConfig):
 
     def ready(self):
         post_migrate.connect(create_roles, sender=self)
+        post_migrate.connect(create_permissions, sender=self)
