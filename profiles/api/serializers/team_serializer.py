51c812d361db505af8bb2b7ed44b0155d735310d
elias-boulharts <elias.boulharts@hpe.com>
2023-07-03 14:07:04 +0200

    TeamAPISerializer data moved in view

--- profiles/api/serializers/team_serializer.py
+++ profiles/api/serializers/team_serializer.py
@@ -10,20 +10,5 @@ class TeamSerializer(ScopeSerializer):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        view = self.context.get('view')
-        self.organization_id = None
-        if view:
-            self.organization_id = view.kwargs.get('organization_id', None)
-            # Change the queryset when the organization is present in url kwargs
-            if self.organization_id:
-                self.fields.fields['org'].queryset = Organization.objects.filter(id=self.organization_id)
-                self.fields.fields['org'].required = False
-                self.fields.fields['org'].initial = self.organization_id
-        else:
-            self.fields.fields.pop('org')
-
-    def is_valid(self, *args, **kwargs):
-        # Override the organization when it is present in url kwargs
-        if self.organization_id:
-            self.initial_data['org'] = self.organization_id
-        return super().is_valid(*args, **kwargs)
+        if kwargs.get('organization_id'):
+            self.fields.fields['org'].queryset = Organization.objects.filter(id=kwargs.get('organization_id'))
