1a5fc4fa580c1310301dc979a2f9773c566f79ab
Nicolas Marcq <nico.marcq@gmail.com>
2023-07-04 11:26:54 +0200

    [Feature] add dark mode

--- profiles/views/__init__.py
+++ profiles/views/__init__.py
@@ -9,3 +9,4 @@ from profiles.views.user_views import *
 from profiles.views.role_views import *
 from profiles.views.globalpermission_views import *
 from profiles.views.quota_views import *
+from profiles.views.profile_views import *
