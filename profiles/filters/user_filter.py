7398288fe84dedc72ff98bb175ce0ba78e52c88c
Nicolas Marcq <nico.marcq@gmail.com>
2023-07-04 11:26:54 +0200

    [Feature] add dark mode

--- profiles/filters/user_filter.py
+++ profiles/filters/user_filter.py
@@ -1,3 +1,5 @@
+import django_filters
+from django import forms
 from django.contrib.auth.models import User
 from Squest.utils.squest_filter import SquestFilter
 
@@ -6,3 +8,12 @@ class UserFilter(SquestFilter):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+    no_organization = django_filters.BooleanFilter(method='has_no_organization',
+                                                   label="No organization",
+                                                   widget=forms.CheckboxInput(attrs={'class': 'form-control-checkbox'}))
+
+    def has_no_organization(self, queryset, field_name, value):
+        if not value:
+            return queryset
+        return queryset.filter(groups__isnull=True)
