58b826872c33f7abd59fa603a1599f4ec3fea105
Anthony Belhadj <abelhadj@hpe.com>
2023-07-20 13:59:14 +0200

    templates refactoring

--- service_catalog/tables/support_tables.py
+++ service_catalog/tables/support_tables.py
@@ -1,13 +1,12 @@
-from django_tables2 import tables, TemplateColumn, LinkColumn
-from django_tables2.utils import A
+from django_tables2 import TemplateColumn, LinkColumn
 
 from service_catalog.models import Support
 from Squest.utils.squest_table import SquestTable
 
 
 class SupportTable(SquestTable):
-    state = TemplateColumn(template_name='custom_columns/support_state.html')
-    date_opened = TemplateColumn(template_name='custom_columns/generic_date_format.html')
+    state = TemplateColumn(template_name='service_catalog/custom_columns/support_state.html')
+    date_opened = TemplateColumn(template_name='generics/custom_columns/generic_date_format.html')
     title = LinkColumn()
     instance = LinkColumn()
 
