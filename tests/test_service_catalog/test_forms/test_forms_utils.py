7298664d8a823f97d7f86fbc5fa6f49dfac1eae0
Nicolas Marcq <nico.marcq@gmail.com>
2023-07-21 15:41:51 +0200

    Fix global hook ajax

--- tests/test_service_catalog/test_forms/test_forms_utils.py
+++ tests/test_service_catalog/test_forms/test_forms_utils.py
@@ -1,4 +1,4 @@
-from service_catalog.forms import utils
+from service_catalog.api.serializers import DynamicSurveySerializer
 from service_catalog.forms.form_utils import FormUtils
 from tests.test_service_catalog.base import BaseTest
 
@@ -15,16 +15,11 @@ class TestServiceRequestForm(BaseTest):
         test_string = "choice1\nchoice2\nchoice3"
         expected_value = [("", "Select an option"), ("choice1", "choice1"), ("choice2", "choice2"),
                           ("choice3", "choice3")]
-        self.assertEqual(expected_value, utils.get_choices_as_tuples_list(test_string))
+        self.assertEqual(expected_value, FormUtils.get_choices_as_tuples_list(test_string))
         test_list = ["choice1", "choice2", "choice3"]
-        self.assertEqual(expected_value, utils.get_choices_as_tuples_list(test_list))
+        self.assertEqual(expected_value, FormUtils.get_choices_as_tuples_list(test_list))
 
     def test_create_form_fields(self):
+        survey = DynamicSurveySerializer(fill_in_survey=self.testing_survey_with_validator)
         expected_result = len(self.testing_survey_with_validator["spec"])
-        self.assertEqual(len(utils.get_fields_from_survey(self.testing_survey_with_validator)), expected_result)
-
-    def test_set_default_fields_values(self):
-        form = utils.get_fields_from_survey(self.testing_survey_with_validator)
-        expected_results = ["", "choice1", ["multiselect_2", "multiselect_3"], "textarea_val", None, 1, 1.5]
-        for field, expected_result in zip(form.values(), expected_results):
-            self.assertEqual(field.initial, expected_result)
+        self.assertEqual(len(survey._get_fields_from_survey()), expected_result)
