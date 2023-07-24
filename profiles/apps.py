d9b46a0e442f0cc5d1970724485cd2e879c13d5f
elias-boulharts <elias.boulharts@hpe.com>
2023-07-24 10:50:02 +0200

    Move default user permissions in apps.py

--- profiles/apps.py
+++ profiles/apps.py
@@ -5,6 +5,42 @@ from django.db.models.signals import post_migrate
 
 logger = logging.getLogger(__name__)
 
+default_user_permissions = [
+    # Lists
+    'service_catalog.list_instance',
+    'service_catalog.list_request',
+    'service_catalog.list_support',
+    'profiles.list_organization',
+    'profiles.list_team',
+    # Custom link
+    'service_catalog.view_customlink',
+    # Doc
+    'service_catalog.list_doc',
+    'service_catalog.view_doc',
+    # Portfolio
+    'service_catalog.list_portfolio',
+    'service_catalog.view_portfolio',
+    # Service
+    'service_catalog.list_service',
+    'service_catalog.view_service',
+    # Operation
+    'service_catalog.list_operation',
+    'service_catalog.view_operation',
+    'service_catalog.request_on_service',
+    # Request notification
+    'profiles.list_requestnotification',
+    'profiles.add_requestnotification',
+    'profiles.view_requestnotification',
+    'profiles.change_requestnotification',
+    'profiles.delete_requestnotification',
+    # Instance notification
+    'profiles.list_instancenotification',
+    'profiles.add_instancenotification',
+    'profiles.view_instancenotification',
+    'profiles.change_instancenotification',
+    'profiles.delete_instancenotification',
+]
+
 
 def create_roles(sender, **kwargs):
     logger.info("create_roles method called")
@@ -24,8 +60,26 @@ def create_roles(sender, **kwargs):
                 role.permissions.add(permission)
 
 
+def insert_default_user_permissions(sender, **kwargs):
+    from profiles.models import GlobalPermission
+    from django.contrib.auth.models import Permission
+    global_permission, created = GlobalPermission.objects.get_or_create(name="GlobalPermission")
+    if created:
+        codenames = list(
+            map(lambda user_permissions: user_permissions.split('.')[1], default_user_permissions))
+        app_labels = list(
+            map(lambda user_permissions: user_permissions.split('.')[0], default_user_permissions))
+        global_permission.user_permissions.add(
+            *Permission.objects.filter(
+                codename__in=codenames,
+                content_type__app_label__in=app_labels
+            )
+        )
+
+
 class ProfilesConfig(AppConfig):
     name = 'profiles'
 
     def ready(self):
         post_migrate.connect(create_roles, sender=self)
+        post_migrate.connect(insert_default_user_permissions, sender=self)
