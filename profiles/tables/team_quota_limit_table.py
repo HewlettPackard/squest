3ecd6903b36284fa5d4abf2ba5c14131539778de
Anthony Belhadj <abelhadj@hpe.com>
2023-07-24 11:33:00 +0200

    Refacto table with LinkColumn

--- profiles/tables/team_quota_limit_table.py
+++ profiles/tables/team_quota_limit_table.py
@@ -1,21 +1,16 @@
 from django.urls import reverse
 from django.utils.html import format_html
-from django_tables2 import tables
+from django_tables2 import LinkColumn, Table
 
 from profiles.models import Quota
 
 
-class TeamQuotaLimitTable(tables.Table):
+class TeamQuotaLimitTable(Table):
+
+    scope = LinkColumn()
+    limit = LinkColumn()
 
     class Meta:
         model = Quota
         attrs = {"id": "quota_team_table", "class": "table squest-pagination-tables"}
         fields = ("scope", "limit")
-
-    def render_limit(self, value, record):
-        link = reverse("profiles:team_quota_edit", kwargs={'scope_id': record.scope.id})
-        return format_html(f'<a href="{link}">{value}</a>')
-
-    def render_scope(self, value, record):
-        link = reverse("profiles:team_details", kwargs={'pk': record.scope.id})
-        return format_html(f'<a href="{link}">{value}</a>')
