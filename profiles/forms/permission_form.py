e82609f1527c47974467b9f3ba8ab6e0f3d0c016
elias-boulharts <elias.boulharts@hpe.com>
2023-07-24 14:27:19 +0200

    Approval step permission

--- profiles/forms/permission_form.py
+++ profiles/forms/permission_form.py
@@ -7,3 +7,9 @@ class PermissionForm(SquestModelForm):
     class Meta:
         model = Permission
         fields = "__all__"
+
+    def __init__(self, content_type, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].help_text = f'A short description of the permission.'
+        self.fields['codename'].help_text = f'Should be split by "_" and finished by the object type (depends on ' \
+                                            f'content type chosen).'
