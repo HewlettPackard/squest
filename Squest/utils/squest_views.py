51374e9f74877cc00a9cdeeb8f265083d156a4d3
elias-boulharts <elias.boulharts@hpe.com>
2023-07-24 10:56:33 +0200

    fix details view

--- Squest/utils/squest_views.py
+++ Squest/utils/squest_views.py
@@ -185,7 +185,7 @@ class SquestDetailView(LoginRequiredMixin, SquestPermissionRequiredMixin, Squest
                 'url': self.get_generic_url('list')
             },
             {
-                'text': f'New {self.django_content_type.name}',
+                'text': self.django_content_type.name,
                 'url': ""
             },
         ]
