f88946e85ab4bc34b57749975d6569e053fe2691
Anthony Belhadj <abelhadj@hpe.com>
2023-07-24 11:33:00 +0200

    Refacto table with LinkColumn

--- profiles/tables/instance_consumption_table.py
+++ profiles/tables/instance_consumption_table.py
@@ -1,17 +1,13 @@
-from django.urls import reverse
-from django.utils.html import format_html
-from django_tables2 import tables
+from django_tables2 import LinkColumn, Table
 
 from resource_tracker_v2.models import ResourceAttribute
 
 
-class InstanceConsumptionTable(tables.Table):
+class InstanceConsumptionTable(Table):
+
+    resource__service_catalog_instance = LinkColumn(verbose_name="Instance")
 
     class Meta:
         model = ResourceAttribute
         attrs = {"id": "instance_consumption_table", "class": "table squest-pagination-tables"}
-        fields = ("resource", "value")
-
-    def render_resource(self, record, value):
-        link = reverse("service_catalog:instance_details", kwargs={'pk': record.resource.service_catalog_instance.id})
-        return format_html(f'<a href="{link}">{record.resource.service_catalog_instance.name}</a>')
+        fields = ("resource__service_catalog_instance", "value")
