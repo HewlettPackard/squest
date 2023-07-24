2253050412fbf1c572fa8be87710220bebd58ccd
elias-boulharts <elias.boulharts@hpe.com>
2023-07-24 16:06:54 +0200

    Use model form for instance in service request

--- service_catalog/forms/service_request_forms.py
+++ service_catalog/forms/service_request_forms.py
@@ -1,30 +1,30 @@
 from django import forms
 
 from Squest.utils.squest_form import SquestForm
+from Squest.utils.squest_model_form import SquestModelForm
 from profiles.models.scope import Scope
 from service_catalog.forms.form_generator import FormGenerator
-
-FIRST_BLOCK_FORM_FIELD_TITTLE = "1. Squest fields"
-EXCLUDED_SURVEY_FIELDS = ["quota_scope", "request_comment", "squest_instance_name"]
+from service_catalog.models import Instance
 
 
-class ServiceRequestForm1(SquestForm):
-    squest_instance_name = forms.CharField(label="Squest instance name",
-                                           help_text="Ideally a short name like 'my-service-01'",
-                                           )
-    quota_scope = forms.ModelChoiceField(
-        label="Quota",
-        queryset=Scope.objects.none(),
-        required=True,
-    )
+class ServiceInstanceForm(SquestModelForm):
+    class Meta:
+        model = Instance
+        fields = ['name', 'quota_scope']
 
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)
-        super(ServiceRequestForm1, self).__init__(*args, **kwargs)
-        self.fields['quota_scope'].queryset = Scope.get_queryset_for_user(user, 'profiles.consume_quota_scope')
+        self.user = kwargs.pop('user', None)
+        super(ServiceInstanceForm, self).__init__(*args, **kwargs)
+        self.fields['quota_scope'].queryset = Scope.get_queryset_for_user(self.user, 'profiles.consume_quota_scope')
+
+    def save(self, commit=True):
+        squest_instance = super().save(False)
+        squest_instance.requester = self.user
+        squest_instance.save()
+        return squest_instance
 
 
-class ServiceRequestForm2(SquestForm):
+class ServiceRequestForm(SquestForm):
     request_comment = forms.CharField(label="Comment",
                                       help_text="Add a comment to your request",
                                       required=False)
@@ -33,9 +33,8 @@ class ServiceRequestForm2(SquestForm):
         self.user = kwargs.pop('user', None)
         self.service = kwargs.pop('service', None)
         self.create_operation = kwargs.pop('operation', None)
-        self.squest_instance_name = kwargs.pop('squest_instance_name', None)
         self.quota_scope = kwargs.pop('quota_scope', None)
-        super(ServiceRequestForm2, self).__init__(*args, **kwargs)
+        super(ServiceRequestForm, self).__init__(*args, **kwargs)
 
         form_generator = FormGenerator(operation=self.create_operation)
         self.fields.update(form_generator.generate_form())
