8a670acfd7e30c6c21ca33c9268c202dd4a016cf
Nicolas Marcq <nico.marcq@gmail.com>
2023-07-26 18:15:18 +0200

    add tests to approval forms
    fix test

--- service_catalog/forms/approval_step_form.py
+++ service_catalog/forms/approval_step_form.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import Permission
 from django.core.exceptions import ValidationError
-from django.forms import ModelChoiceField, ModelMultipleChoiceField, HiddenInput
+from django.forms import ModelMultipleChoiceField, HiddenInput
 from django.urls import reverse
 
 from Squest.utils.squest_model_form import SquestModelForm
@@ -32,16 +32,6 @@ class ApprovalStepForm(SquestModelForm):
         self.fields['permission'].help_text = f"Create a <a href='{reverse('profiles:approvalstep_permission_create')}'>permission</a>"
         default_perm = Permission.objects.get(codename="can_approve_approvalstep")
         self.fields['permission'].initial = (default_perm.id, default_perm.name)
-
-    # def save(self, commit=True):
-    #     self.instance = super(ApprovalStepForm, self).save(commit=False)
-    #     previous_steps = ApprovalStep.objects.filter(approval_workflow=self.instance.approval_workflow).order_by('position')
-    #     if previous_steps.exists():
-    #         self.instance.position = previous_steps.last().position + 1
-    #     if commit:
-    #         self.instance.save()
-    #         self.save_m2m()
-    #     return self.instance
 
     def clean(self):
         cleaned_data = super().clean()
