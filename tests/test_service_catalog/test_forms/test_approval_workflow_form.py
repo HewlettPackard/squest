84401cacbd8289979032af96ca212e1ff5c8bcc3
Anthony Belhadj <abelhadj@hpe.com>
2023-07-31 14:48:16 +0200

    Bug fix ApprovalWorkflow (add scope to existing workflow)

--- tests/test_service_catalog/test_forms/test_approval_workflow_form.py
+++ tests/test_service_catalog/test_forms/test_approval_workflow_form.py
@@ -18,9 +18,9 @@ class TestApprovalWorkflowForm(BaseTest):
         self.assertTrue(form.is_valid())
 
     def test_clean(self):
-        exiting_approval = ApprovalWorkflow.objects.create(name="test",
+        existing_approval = ApprovalWorkflow.objects.create(name="test",
                                                            operation=self.create_operation_test)
-        exiting_approval.scopes.set([self.test_quota_scope])
+        existing_approval.scopes.set([self.test_quota_scope])
 
         data = {
             'name': 'test_approval_workflow',
@@ -30,3 +30,17 @@ class TestApprovalWorkflowForm(BaseTest):
         form = ApprovalWorkflowForm(data)
         self.assertFalse(form.is_valid())
         self.assertIn("has already an approval workflow", form["scopes"].errors[0])
+
+
+    def test_add_scope_to_scopes(self):
+        existing_approval = ApprovalWorkflow.objects.create(name="test",
+                                                           operation=self.create_operation_test)
+        existing_approval.scopes.set([self.test_quota_scope])
+
+        data = {
+            'name': 'test_approval_workflow',
+            'operation': self.create_operation_test,
+            'scopes': [self.test_quota_scope,self.test_quota_scope2]
+        }
+        form = ApprovalWorkflowForm(instance=existing_approval,data=data)
+        self.assertTrue(form.is_valid())
