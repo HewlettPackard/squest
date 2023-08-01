c514f1e57229d2d84eb9be23185278b673624815
Nicolas Marcq <nico.marcq@gmail.com>
2023-08-01 10:55:03 +0200

    can delete a quota from a scope + update Form generator to set max value

--- profiles/tables/quota_table.py
+++ profiles/tables/quota_table.py
@@ -1,5 +1,5 @@
 from django.utils.html import format_html
-from django_tables2 import LinkColumn, Table
+from django_tables2 import LinkColumn, Table, TemplateColumn
 
 from Squest.utils.squest_table import SquestTable
 from profiles.models import Quota
@@ -8,11 +8,12 @@ from profiles.models import Quota
 class QuotaTable(SquestTable):
     scope = LinkColumn()
     attribute_definition = LinkColumn()
+    actions = TemplateColumn(template_name='generics/custom_columns/generic_delete.html', orderable=False)
 
     class Meta:
         model = Quota
         attrs = {"id": "quota_table", "class": "table squest-pagination-tables "}
-        fields = ("scope", "attribute_definition", "limit", "consumed", "available")
+        fields = ("scope", "attribute_definition", "limit", "consumed", "available", "action")
 
     def render_consumed(self, value, record):
         link = record.get_absolute_url()
