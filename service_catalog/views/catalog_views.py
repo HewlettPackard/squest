41894d46fce8a22a39f4d4550125af404ec1e6c6
Nicolas Marcq <nico.marcq@gmail.com>
2023-08-02 16:00:55 +0200

    block service request from api if limit quota reached

--- service_catalog/views/catalog_views.py
+++ service_catalog/views/catalog_views.py
@@ -5,8 +5,6 @@ from Squest.utils.squest_rbac import SquestPermissionRequiredMixin
 from profiles.models import Scope
 from service_catalog.models import Service, OperationType, Instance, Operation, Request, RequestMessage
 
-EXCLUDED_SURVEY_FIELDS = ["request_comment"]
-
 
 class ServiceRequestWizardView(SquestPermissionRequiredMixin, SessionWizardView):
 
@@ -25,46 +23,22 @@ class ServiceRequestWizardView(SquestPermissionRequiredMixin, SessionWizardView)
 
     def get_form_kwargs(self, step):
         kwargs = super().get_form_kwargs()
-        kwargs.update({'user': self.request.user, 'service': self.service})
+        kwargs.update({'user': self.request.user})
+        kwargs.update({'operation': self.operation})
 
         if step == "1":
-            kwargs.update({'operation': self.operation})
             # add data from step 0
             scope_id = self.storage.data['step_data']['0']['0-quota_scope'][0]
-            get_object_or_404(Scope, id=scope_id)
-            kwargs.update({'quota_scope': scope_id})
+            quota_scope = get_object_or_404(Scope, id=scope_id)
+            kwargs.update({'quota_scope': quota_scope})
         return kwargs
 
     def get_template_names(self):
         return "service_catalog/generic_form_multiple_step.html"
 
     def done(self, form_list, **kwargs):
-        service_id = self.kwargs['service_id']
-        operation_id = self.kwargs['operation_id']
-        service = get_object_or_404(Service, **{'id': service_id, 'enabled': True})
-        create_operation = get_object_or_404(Operation, id=operation_id)
-
         # get data from the first form
         new_instance = form_list[0].save()
-
         # get data from the second form
-        comment = form_list[1].cleaned_data["request_comment"]
-        user_provided_survey_fields = dict()
-        for field_key, value in form_list[1].cleaned_data.items():
-            if field_key not in EXCLUDED_SURVEY_FIELDS:
-                user_provided_survey_fields[field_key] = value
-
-        # create the request
-        new_request = Request.objects.create(instance=new_instance,
-                                             operation=create_operation,
-                                             fill_in_survey=user_provided_survey_fields,
-                                             user=self.request.user)
-
-        # save the comment
-        message = None
-        if comment is not None and comment != "":
-            message = RequestMessage.objects.create(request=new_request, sender=self.request.user, content=comment)
-        from service_catalog.mail_utils import send_mail_request_update
-        send_mail_request_update(target_request=new_request, user_applied_state=new_request.user, message=message)
-
+        form_list[1].save(new_instance)
         return redirect('service_catalog:request_list')
