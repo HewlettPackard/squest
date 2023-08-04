8b416e250ac1fb51894f866f917a871fbcb06897
Nicolas Marcq <nico.marcq@gmail.com>
2023-08-04 17:13:56 +0200

    split default perm and global perm rbac

--- profiles/views/globalpermission.py
+++ profiles/views/globalpermission.py
@@ -9,6 +9,9 @@ from profiles.tables import UserRoleTable, PermissionTable
 class GlobalPermissionDetailView(SquestDetailView):
     model = GlobalPermission
 
+    def get_permission_required(self):
+        return "profiles.view_users_globalpermission"
+
     def get_object(self, queryset=None):
         return GlobalPermission.load()
 
@@ -20,11 +23,29 @@ class GlobalPermissionDetailView(SquestDetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['breadcrumbs'] = None
-        if self.request.user.has_perm("profiles.view_users_globalpermission", self.get_object()):
-            context['users'] = UserRoleTable(self.object.users)
+        context['title'] = "Global permission"
+        context['users'] = UserRoleTable(self.object.users)
+        return context
+
+
+class GlobalPermissionDefaultPermissionView(SquestDetailView):
+    model = GlobalPermission
+    template_name = "profiles/default_permission_detail.html"
+
+    def get_object(self, queryset=None):
+        return GlobalPermission.load()
+
+    def dispatch(self, request, *args, **kwargs):
+        self.kwargs['pk'] = self.get_object().id
+        kwargs['pk'] = self.kwargs.get('pk')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['breadcrumbs'] = None
+        context['title'] = "Default permissions"
         permission_table = PermissionTable(self.object.default_permissions.all())
         permission_table.exclude = ("actions",)
-
         context['default_permissions'] = permission_table
         return context
 
