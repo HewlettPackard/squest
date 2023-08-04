8f634146749e56a42fa93c2e30f9512f79032549
elias-boulharts <elias.boulharts@hpe.com>
2023-08-04 13:54:05 +0200

    Test resource group details

--- resource_tracker_v2/tables/resource_table.py
+++ resource_tracker_v2/tables/resource_table.py
@@ -10,10 +10,18 @@ class ResourceTable(SquestTable):
     class Meta:
         model = Resource
         attrs = {"id": "resource_table", "class": "table squest-pagination-tables"}
+        fields = ("selection", "name", "service_catalog_instance", "actions")
+
+    selection = CheckBoxColumn(accessor='pk', attrs={"th__input": {"onclick": "toggle(this)"}})
+    service_catalog_instance = LinkColumn(verbose_name="Instance")
+    actions = TemplateColumn(
+        template_name='resource_tracker_v2/resource_group/resources/custom_columns/resource_operations.html',
+        orderable=False
+    )
 
     def __init__(self, *args, **kwargs):
         super(ResourceTable, self).__init__(*args, **kwargs)
-        self.base_columns.clear()
+        # self.base_columns.clear()
         self.columns = BoundColumns(self, type(self).base_columns)
         if self.data.data.count() != 0:
             resource_group = self.data.data.first().resource_group
@@ -23,15 +31,10 @@ class ResourceTable(SquestTable):
             sequence = [*["selection", "name", "service_catalog_instance"], *attribute_names, *["actions"]]
             self.Meta.fields = tuple(sequence)
             self.sequence = sequence
-            type(self).base_columns["selection"] = CheckBoxColumn(accessor='pk', attrs={"th__input": {"onclick": "toggle(this)"}})
-            type(self).base_columns["name"] = Column()
-            type(self).base_columns["service_catalog_instance"] = LinkColumn("service_catalog:instance_details", args=[A("service_catalog_instance__id")], verbose_name="Instance")
             for attribute_name in attribute_names:
                 type(self).base_columns[attribute_name] = TemplateColumn(
                     template_name='resource_tracker_v2/resource_group/custom_columns/attribute_value.html',
                     extra_context={'attribute_name': attribute_name},
                     orderable=False
                 )
-            type(self).base_columns["actions"] = TemplateColumn(template_name='resource_tracker_v2/resource_group/resources/custom_columns/resource_operations.html',
-                                                                orderable=False)
             self.columns = BoundColumns(self, type(self).base_columns)
