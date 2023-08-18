994ae956c6a40107c25837ca53fd94e3ddd52d2d
elias-boulharts <elias.boulharts@hpe.com>
2023-08-18 12:26:20 +0200

    Remove resource_group from editable fields in Resource and Transformer

--- resource_tracker_v2/api/views/resource_api_view.py
+++ resource_tracker_v2/api/views/resource_api_view.py
@@ -1,11 +1,7 @@
-import copy
-
 from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 
 from Squest.utils.squest_api_views import SquestListCreateAPIView, SquestRetrieveUpdateDestroyAPIView
-from resource_tracker_v2.api.serializers.resource_serializer import ResourceSerializer, ResourceCreateSerializer
+from resource_tracker_v2.api.serializers.resource_serializer import ResourceSerializer
 from resource_tracker_v2.filters.resource_filter import ResourceFilter
 from resource_tracker_v2.models import Resource, ResourceGroup
 
@@ -19,17 +15,11 @@ class ResourceListCreate(SquestListCreateAPIView):
             return Resource.objects.none()
         return Resource.objects.filter(resource_group_id=self.kwargs['resource_group_id'])
 
-    def create(self, request, **kwargs):
-        resource_group_id = self.kwargs['resource_group_id']
-        resource_group = get_object_or_404(ResourceGroup, pk=resource_group_id)
-        data = copy.copy(request.data)
-        data["resource_group"] = resource_group.id
-        serializer = ResourceCreateSerializer(data=data)
-        if serializer.is_valid():
-            new_resource = serializer.save()
-            read_serializer = ResourceSerializer(instance=new_resource)
-            return Response(read_serializer.data, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+    def get_serializer_context(self):
+        context = super(ResourceListCreate, self).get_serializer_context()
+        resource_group = get_object_or_404(ResourceGroup, pk=self.kwargs['resource_group_id'])
+        context["resource_group"] = resource_group
+        return context
 
 
 class ResourceDetails(SquestRetrieveUpdateDestroyAPIView):
@@ -40,15 +30,8 @@ class ResourceDetails(SquestRetrieveUpdateDestroyAPIView):
             return Resource.objects.none()
         return Resource.objects.filter(resource_group_id=self.kwargs['resource_group_id'])
 
-    def get_serializer(self, *args, **kwargs):
-        if 'data' in kwargs:
-            if kwargs['data'].get('resource_group') is None:
-                if hasattr(kwargs['data'], '_mutable'):
-                    is_mutable = kwargs['data']._mutable
-                    kwargs['data']._mutable = True
-                kwargs['data']['resource_group'] = self.kwargs.get('resource_group_id', None)
-                if hasattr(kwargs['data'], '_mutable'):
-                    kwargs['data']._mutable = is_mutable
-        serializer = super(ResourceDetails, self).get_serializer(*args, **kwargs)
-        serializer.context["resource_group"] = self.kwargs.get('resource_group_id', None)
-        return serializer
+    def get_serializer_context(self):
+        context = super(ResourceDetails, self).get_serializer_context()
+        resource_group = get_object_or_404(ResourceGroup, pk=self.kwargs['resource_group_id'])
+        context["resource_group"] = resource_group
+        return context
