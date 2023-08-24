b88aee9ceac7b0ef842296b2195e4459e59dac06
elias-boulharts <elias.boulharts@hpe.com>
2023-08-24 18:48:29 +0200

    Remove plural in URLs

--- resource_tracker_v2/urls.py
+++ resource_tracker_v2/urls.py
@@ -6,11 +6,11 @@ app_name = 'resource_tracker_v2'
 
 urlpatterns = [
     # Attribute definition CRUD
-    path('attributes/', views.AttributeDefinitionListView.as_view(), name='attributedefinition_list'),
-    path('attributes/create/', views.AttributeDefinitionCreateView.as_view(), name='attributedefinition_create'),
-    path('attributes/<int:pk>/', views.AttributeDefinitionDetailView.as_view(), name='attributedefinition_details'),
-    path('attributes/<int:pk>/edit/', views.AttributeDefinitionEditView.as_view(), name='attributedefinition_edit'),
-    path('attributes/<int:pk>/delete/', views.AttributeDefinitionDeleteView.as_view(), name='attributedefinition_delete'),
+    path('attribute/', views.AttributeDefinitionListView.as_view(), name='attributedefinition_list'),
+    path('attribute/create/', views.AttributeDefinitionCreateView.as_view(), name='attributedefinition_create'),
+    path('attribute/<int:pk>/', views.AttributeDefinitionDetailView.as_view(), name='attributedefinition_details'),
+    path('attribute/<int:pk>/edit/', views.AttributeDefinitionEditView.as_view(), name='attributedefinition_edit'),
+    path('attribute/<int:pk>/delete/', views.AttributeDefinitionDeleteView.as_view(), name='attributedefinition_delete'),
 
     # Resource group CRUD
     path('resource_group/', views.ResourceGroupListView.as_view(), name='resourcegroup_list'),
@@ -23,19 +23,19 @@ urlpatterns = [
     path('resource_tracker_graph/', views.resource_tracker_graph, name='resource_tracker_graph'),
 
     # Transformer CRUD
-    path('resource_group/<int:resource_group_id>/attributes/', views.TransformerListView.as_view(), name='transformer_list'),
-    path('resource_group/<int:resource_group_id>/attributes/create/', views.TransformerCreateView.as_view(), name='transformer_create'),
-    path('resource_group/<int:resource_group_id>/attributes/<int:attribute_id>/edit/', views.TransformerEditView.as_view(), name='transformer_edit'),
-    path('resource_group/<int:resource_group_id>/attributes/<int:attribute_id>/delete/', views.TransformerDeleteView.as_view(), name='transformer_delete'),
+    path('resource_group/<int:resource_group_id>/attribute/', views.TransformerListView.as_view(), name='transformer_list'),
+    path('resource_group/<int:resource_group_id>/attribute/create/', views.TransformerCreateView.as_view(), name='transformer_create'),
+    path('resource_group/<int:resource_group_id>/attribute/<int:attribute_id>/edit/', views.TransformerEditView.as_view(), name='transformer_edit'),
+    path('resource_group/<int:resource_group_id>/attribute/<int:attribute_id>/delete/', views.TransformerDeleteView.as_view(), name='transformer_delete'),
     # Transformer AJAX
     path('resource_group/load-resource-group-attribute/', views.ajax_load_attribute, name='ajax_load_attribute'),
 
     # Resource CRUD
-    path('resource_group/<int:resource_group_id>/resources/', views.ResourceListView.as_view(), name='resource_list'),
-    path('resource_group/<int:resource_group_id>/resources/create/', views.ResourceCreateView.as_view(), name='resource_create'),
-    path('resource_group/<int:resource_group_id>/resources/<int:pk>/edit/', views.ResourceEditView.as_view(), name='resource_edit'),
-    path('resource_group/<int:resource_group_id>/resources/<int:pk>/delete/', views.ResourceDeleteView.as_view(), name='resource_delete'),
+    path('resource_group/<int:resource_group_id>/resource/', views.ResourceListView.as_view(), name='resource_list'),
+    path('resource_group/<int:resource_group_id>/resource/create/', views.ResourceCreateView.as_view(), name='resource_create'),
+    path('resource_group/<int:resource_group_id>/resource/<int:pk>/edit/', views.ResourceEditView.as_view(), name='resource_edit'),
+    path('resource_group/<int:resource_group_id>/resource/<int:pk>/delete/', views.ResourceDeleteView.as_view(), name='resource_delete'),
     # Resource particular urls
-    path('resource_group/<int:resource_group_id>/resources/<int:pk>/move/', views.ResourceMoveView.as_view(), name='resource_move'),
-    path('resource_group/<int:resource_group_id>/resources/delete/', views.resource_group_resource_bulk_delete, name='resource_bulk_delete')
+    path('resource_group/<int:resource_group_id>/resource/<int:pk>/move/', views.ResourceMoveView.as_view(), name='resource_move'),
+    path('resource_group/<int:resource_group_id>/resource/delete/', views.resource_group_resource_bulk_delete, name='resource_bulk_delete')
 ]
