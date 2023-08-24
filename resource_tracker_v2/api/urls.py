488afe4aa58b030eaf3a69120bf07bb8d62e3dcb
elias-boulharts <elias.boulharts@hpe.com>
2023-08-24 18:48:29 +0200

    Remove plural in URLs

--- resource_tracker_v2/api/urls.py
+++ resource_tracker_v2/api/urls.py
@@ -8,25 +8,25 @@ from resource_tracker_v2.api.views.transformer_api_views import TransformerListC
 
 urlpatterns = [
     # attribute definition
-    path('attributes/', AttributeDefinitionList.as_view(), name='api_attributedefinition_list_create'),
-    path('attributes/<int:pk>/', AttributeDefinitionDetails.as_view(), name='api_attributedefinition_details'),
+    path('attribute/', AttributeDefinitionList.as_view(), name='api_attributedefinition_list_create'),
+    path('attribute/<int:pk>/', AttributeDefinitionDetails.as_view(), name='api_attributedefinition_details'),
 
     # resource group
     path('resource_group/', ResourceGroupList.as_view(), name='api_resourcegroup_list_create'),
     path('resource_group/<int:pk>/', ResourceGroupDetails.as_view(), name='api_resourcegroup_details'),
 
     # resource
-    path('resource_group/<int:resource_group_id>/resources/', ResourceListCreate.as_view(),
+    path('resource_group/<int:resource_group_id>/resource/', ResourceListCreate.as_view(),
          name='api_resource_list_create'),
-    path('resource_group/<int:resource_group_id>/resources/<int:pk>/', ResourceDetails.as_view(),
+    path('resource_group/<int:resource_group_id>/resource/<int:pk>/', ResourceDetails.as_view(),
          name='api_resource_details'),
-    # path('resource_group/<int:resource_group_id>/resources/<int:pk>/move/', ResourceMove.as_view(),
+    # path('resource_group/<int:resource_group_id>/resource/<int:pk>/move/', ResourceMove.as_view(),
     #      name='api_resource_move'),
 
     # transformer
-    path('resource_group/<int:resource_group_id>/attributes/', TransformerListCreate.as_view(),
+    path('resource_group/<int:resource_group_id>/attribute/', TransformerListCreate.as_view(),
          name='api_transformer_list_create'),
-    path('resource_group/<int:resource_group_id>/attributes/<int:pk>/', TransformerDetails.as_view(),
+    path('resource_group/<int:resource_group_id>/attribute/<int:pk>/', TransformerDetails.as_view(),
          name='api_transformer_details'),
 
 ]
