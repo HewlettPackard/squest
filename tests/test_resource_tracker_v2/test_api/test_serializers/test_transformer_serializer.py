0a50c21b987819d5644d181a1d359bbafd3c1922
elias-boulharts <elias.boulharts@hpe.com>
2023-08-30 12:29:22 +0200

    transformer api update

--- tests/test_resource_tracker_v2/test_api/test_serializers/test_transformer_serializer.py
+++ tests/test_resource_tracker_v2/test_api/test_serializers/test_transformer_serializer.py
@@ -13,8 +13,8 @@ class TransformerSerializerTests(BaseTestResourceTrackerV2API):
         self.new_attribute = AttributeDefinition.objects.create(name="new_attribute")
         self.new_transformer = Transformer.objects.create(resource_group=self.new_rg,
                                                           attribute_definition=self.core_attribute)
-        self.context = {
-            "resource_group": self.cluster
+        self.data = {
+            "resource_group": self.cluster.id
         }
         self.number_transformer_before = Transformer.objects.all().count()
 
@@ -22,67 +22,52 @@ class TransformerSerializerTests(BaseTestResourceTrackerV2API):
         self.assertEqual(self.number_transformer_before + 1, Transformer.objects.all().count())
 
     def test_create_transformer_no_consumption(self):
-        data = {
-            "attribute_definition": self.new_attribute.id,
-            "consume_from_resource_group": None,
-            "consume_from_attribute_definition": None
-        }
-        serializer = TransformerSerializer(data=data, context=self.context)
+        self.data["attribute_definition"] = self.new_attribute.id
+        self.data["consume_from_resource_group"] = None
+        self.data["consume_from_attribute_definition"] = None
+        serializer = TransformerSerializer(data=self.data)
         self.assertTrue(serializer.is_valid())
         serializer.save()
         self._validate_transformer_created()
 
     def test_create_transformer_with_consumption(self):
-        data = {
-            "attribute_definition": self.new_attribute.id,
-            "consume_from_resource_group": self.new_rg.id,
-            "consume_from_attribute_definition": self.core_attribute.id,
-        }
-        serializer = TransformerSerializer(data=data, context=self.context)
+        self.data["attribute_definition"] = self.new_attribute.id
+        self.data["consume_from_resource_group"] = self.new_rg.id
+        self.data["consume_from_attribute_definition"] = self.core_attribute.id
+        serializer = TransformerSerializer(data=self.data)
         self.assertTrue(serializer.is_valid())
         serializer.save()
         self._validate_transformer_created()
 
     def test_cannot_create_transformer_with_target_rg_and_no_target_attribute(self):
-        data = {
-            "attribute_definition": self.new_attribute.id,
-            "consume_from_resource_group": self.new_rg.id,
-            "consume_from_attribute_definition": None,
-        }
-        serializer = TransformerSerializer(data=data, context=self.context)
+        self.data["attribute_definition"] = self.new_attribute.id
+        self.data["consume_from_resource_group"] = self.new_rg.id
+        self.data["consume_from_attribute_definition"] = None
+        serializer = TransformerSerializer(data=self.data)
         self.assertFalse(serializer.is_valid())
         self.assertEqual(set(serializer.errors.keys()), {'consume_from_attribute_definition'})
 
     def test_cannot_create_transformer_with_not_target_rg_and_target_attribute(self):
-        data = {
-            "attribute_definition": self.new_attribute.id,
-            "consume_from_resource_group": None,
-            "consume_from_attribute_definition": self.new_attribute.id,
-        }
-        data.update(context=self.context)
-        serializer = TransformerSerializer(data=data, context=self.context)
+        self.data["attribute_definition"] = self.new_attribute.id
+        self.data["consume_from_resource_group"] = None
+        self.data["consume_from_attribute_definition"] = self.new_attribute.id
+        serializer = TransformerSerializer(data=self.data)
         self.assertFalse(serializer.is_valid())
         self.assertEqual(set(serializer.errors.keys()), {'consume_from_resource_group'})
 
     def test_transformer_exist_already_on_selected_attribute(self):
-        data = {
-            "attribute_definition": self.core_attribute.id,
-            "consume_from_resource_group": None,
-            "consume_from_attribute_definition": None,
-        }
-        data.update(context=self.context)
-        serializer = TransformerSerializer(data=data, context=self.context)
+        self.data["attribute_definition"] = self.core_attribute.id
+        self.data["consume_from_resource_group"] = None
+        self.data["consume_from_attribute_definition"] = None
+        serializer = TransformerSerializer(data=self.data)
         self.assertFalse(serializer.is_valid())
-        self.assertIn(f"A Transformer already exist for {self.core_attribute} in {self.cluster}", serializer.errors["attribute_definition"][0])
+        self.assertIn(f"The fields resource_group, attribute_definition must make a unique set.", serializer.errors["non_field_errors"][0])
 
     def test_invalid_target_attribute_in_transformer(self):
-        data = {
-            "attribute_definition": self.new_attribute.id,
-            "consume_from_resource_group": self.new_rg.id,
-            "consume_from_attribute_definition": self.vcpu_attribute.id,
-        }
-        data.update(context=self.context)
-        serializer = TransformerSerializer(data=data, context=self.context)
+        self.data["attribute_definition"] = self.new_attribute.id
+        self.data["consume_from_resource_group"] = self.new_rg.id
+        self.data["consume_from_attribute_definition"] = self.vcpu_attribute.id
+        serializer = TransformerSerializer(data=self.data)
         self.assertFalse(serializer.is_valid())
         self.assertEqual(set(serializer.errors.keys()), {'consume_from_attribute'})
 
@@ -90,36 +75,30 @@ class TransformerSerializerTests(BaseTestResourceTrackerV2API):
         other_vms = ResourceGroup.objects.create(name="other_vms")
         Transformer.objects.create(resource_group=other_vms, attribute_definition=self.vcpu_attribute)
         other_field = AttributeDefinition.objects.create(name="other_field")
-        data = {
-            "attribute_definition": other_field.id,
-            "consume_from_resource_group": other_vms.id,
-            "consume_from_attribute_definition": self.vcpu_attribute.id,
-        }
-        self.context['resource_group_id'] = self.single_vms.id
-        serializer = TransformerSerializer(data=data, context=self.context)
+        self.data["attribute_definition"] = other_field.id
+        self.data["consume_from_resource_group"] = other_vms.id
+        self.data["consume_from_attribute_definition"] = self.vcpu_attribute.id
+        self.data['resource_group'] = self.single_vms.id
+        serializer = TransformerSerializer(data=self.data)
         self.assertTrue(serializer.is_valid())
         self._validate_transformer_created()
 
     def test_cannot_create_circular_loop_when_adding_consumer(self):
-        data = {
-            "attribute_definition": self.core_attribute.id,
-            "consume_from_resource_group": self.ocp_projects.id,
-            "consume_from_attribute_definition": self.request_cpu.id,
-        }
-        serializer = TransformerSerializer(instance=self.core_transformer, data=data, context=self.context)
+        self.data["attribute_definition"] = self.core_attribute.id
+        self.data["consume_from_resource_group"] = self.ocp_projects.id
+        self.data["consume_from_attribute_definition"] = self.request_cpu.id
+        serializer = TransformerSerializer(instance=self.core_transformer, data=self.data)
         self.assertFalse(serializer.is_valid())
         self.assertEqual(set(serializer.errors.keys()), {'consume_from_attribute_definition'})
 
     def test_update_factor_also_update_parent_consumption(self):
         consumption_before = self.core_transformer.total_consumed
-        data = {
-            "attribute_definition": self.vcpu_attribute.id,
-            "consume_from_resource_group": self.cluster.id,
-            "consume_from_attribute_definition": self.core_attribute.id,
-            "factor": 2
-        }
-        self.context['resource_group_id'] = self.single_vms.id
-        serializer = TransformerSerializer(instance=self.vcpu_from_core_transformer, data=data, context=self.context)
+        self.data["attribute_definition"] = self.vcpu_attribute.id
+        self.data["consume_from_resource_group"] = self.cluster.id
+        self.data["consume_from_attribute_definition"] = self.core_attribute.id
+        self.data["factor"] = 2
+        self.data['resource_group'] = self.single_vms.id
+        serializer = TransformerSerializer(instance=self.vcpu_from_core_transformer, data=self.data)
         self.assertTrue(serializer.is_valid())
         serializer.save()
         self.core_transformer.refresh_from_db()
