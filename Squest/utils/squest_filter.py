c1fe267d53e467dd85e3799b299302e6abde87f4
Anthony Belhadj <abelhadj@hpe.com>
2023-09-12 17:08:17 +0200

    Add per_page in all forms

--- Squest/utils/squest_filter.py
+++ Squest/utils/squest_filter.py
@@ -1,18 +1,38 @@
+import django_filters
 from django_filters import FilterSet
 from django.forms import TextInput, Select, SelectMultiple, CheckboxInput
 
 
 class SquestFilter(FilterSet):
+    per_page = django_filters.NumberFilter(
+        field_name="per_page",
+        label="Number of elements displayed"
+    )
 
     def __init__(self, *args, **kwargs):
         super(SquestFilter, self).__init__(*args, **kwargs)
+        # Per page at the end of form
+        per_page_filter = self.filters.pop("per_page")
+        per_page_filter.field.widget = Select(
+            choices=[
+                (25, '25'),
+                (50, '50'),
+                (100, '100'),
+                (500, '250'),
+                (500, '500'),
+                (1000, '1000'),
+            ]
+        )
+        self.filters['per_page'] = per_page_filter
+
         for filter_name, current_filter in self.filters.items():
             if isinstance(current_filter.field.widget, TextInput):
                 current_filter.field.widget.attrs['class'] = 'form-control'
                 current_filter.lookup_expr = 'icontains'
                 if self.request and '/api/' in self.request.path:
                     current_filter.lookup_expr = 'exact'
-            elif isinstance(current_filter.field.widget, Select) or isinstance(current_filter.field.widget, SelectMultiple):
+            elif isinstance(current_filter.field.widget, Select) or isinstance(current_filter.field.widget,
+                                                                               SelectMultiple):
                 current_filter.field.widget.attrs['class'] = 'form-control selectpicker'
                 current_filter.field.widget.attrs['data-live-search'] = 'true'
             elif isinstance(current_filter.field.widget, CheckboxInput):
@@ -22,3 +42,7 @@ class SquestFilter(FilterSet):
 
     def fake_filter_method(self, queryset, name, value):
         return queryset
+
+    def filter_queryset(self, queryset):
+        self.form.cleaned_data.pop("per_page")
+        return super().filter_queryset(queryset)
