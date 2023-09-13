f7c90629bc8ebfa5bb2a1c2e9533527091d95d6a
Anthony Belhadj <abelhadj@hpe.com>
2023-09-13 20:59:09 +0200

    Fix error when AnsibleWhen is empty on ApprovalStep

--- Squest/utils/ansible_when.py
+++ Squest/utils/ansible_when.py
@@ -1,13 +1,15 @@
-from jinja2 import Template, UndefinedError
+from jinja2 import Template, UndefinedError, TemplateSyntaxError
 
 
 class AnsibleWhen(object):
 
     @classmethod
     def when_render(cls, context, when_string):
-
-        template_string = "{% if " + when_string + " %}True{% else %}{% endif %}"
-        template = Template(template_string)
+        try:
+            template_string = "{% if " + when_string + " %}True{% else %}{% endif %}"
+            template = Template(template_string)
+        except TemplateSyntaxError:
+            return False
         try:
             template_rendered = template.render(context)
             return bool(template_rendered)
