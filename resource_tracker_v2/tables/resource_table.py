25dfea2c73bc9614c0cff68f55dd0e0c89a6fb11
Nicolas Marcq <nico.marcq@gmail.com>
2023-09-14 14:38:49 +0200

    Fix resource table

--- resource_tracker_v2/tables/resource_table.py
+++ resource_tracker_v2/tables/resource_table.py
@@ -10,31 +10,24 @@ class ResourceTable(SquestTable):
     class Meta:
         model = Resource
         attrs = {"id": "resource_table", "class": "table squest-pagination-tables"}
-        fields = ("selection", "name", "service_catalog_instance", "actions")
+        fields = ("selection", "name", "service_catalog_instance")
 
     selection = CheckBoxColumn(accessor='pk', attrs={"th__input": {"onclick": "toggle(this)"}})
     service_catalog_instance = LinkColumn(verbose_name="Instance")
-    actions = TemplateColumn(
-        template_name='resource_tracker_v2/resource_group/resources/custom_columns/resource_operations.html',
-        orderable=False
-    )
 
     def __init__(self, *args, **kwargs):
+        kwargs["extra_columns"] = list()
+        if "data" in kwargs.keys():
+            if kwargs["data"].count() != 0:
+                resource_group = kwargs["data"].first().resource_group
+                for attribute_name in resource_group.transformers.values_list('attribute_definition__name', flat=True):
+                    kwargs["extra_columns"].append((attribute_name, TemplateColumn(
+                        template_name='resource_tracker_v2/resource_group/custom_columns/attribute_value.html',
+                        extra_context={'attribute_name': attribute_name},
+                        orderable=False
+                    )))
+        kwargs["extra_columns"].append(("Actions", TemplateColumn(
+            template_name='resource_tracker_v2/resource_group/resources/custom_columns/resource_operations.html',
+            orderable=False
+        )))
         super(ResourceTable, self).__init__(*args, **kwargs)
-        # self.base_columns.clear()
-        self.columns = BoundColumns(self, type(self).base_columns)
-        if self.data.data.count() != 0:
-            resource_group = self.data.data.first().resource_group
-            attribute_names = list()
-            for transformer in resource_group.transformers.all():
-                attribute_names.append(transformer.attribute_definition.name)
-            sequence = [*["selection", "name", "service_catalog_instance"], *attribute_names, *["actions"]]
-            self.Meta.fields = tuple(sequence)
-            self.sequence = sequence
-            for attribute_name in attribute_names:
-                type(self).base_columns[attribute_name] = TemplateColumn(
-                    template_name='resource_tracker_v2/resource_group/custom_columns/attribute_value.html',
-                    extra_context={'attribute_name': attribute_name},
-                    orderable=False
-                )
-            self.columns = BoundColumns(self, type(self).base_columns)
