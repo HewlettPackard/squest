71bf895f189ab80882d075b54c72ed42587a92df
Anthony Belhadj <abelhadj@hpe.com>
2023-09-14 18:06:34 +0200

    request list perf

--- service_catalog/tables/request_tables.py
+++ service_catalog/tables/request_tables.py
@@ -9,27 +9,24 @@ class RequestTable(SquestTable):
     selection = CheckBoxColumn(accessor='pk', attrs={"th__input": {"onclick": "toggle(this)"}})
     id = Column(linkify=True, verbose_name="Request")
     date_submitted = TemplateColumn(template_name='generics/custom_columns/generic_date_format.html')
-    instance__quota_scope__name = Column(linkify=True, verbose_name="Quota scope")
-    operation = LinkColumn()
     instance = LinkColumn()
     last_updated = TemplateColumn(template_name='generics/custom_columns/generic_date_format.html')
+    instance__quota_scope__name = Column(verbose_name="Quota scope")
 
     def before_render(self, request):
         if not request.user.has_perm('service_catalog.delete_request'):
             self.columns.hide('selection')
 
-    def render_id(self, value, record):
-        return format_html(f'<a title={value} href="{record.get_absolute_url()}">{record}</a>')
-
-    def render_instance__quota_scope__name(self, value, record):
-        return format_html(f'<a title={value} href="{record.instance.quota_scope.get_absolute_url()}">{value}</a>')
-
     class Meta:
         model = Request
         attrs = {"id": "request_table", "class": "table squest-pagination-tables"}
-        fields = ("selection", "id",  "user__username", "instance__quota_scope__name",
+        fields = ("selection", "id", "user__username", "instance__quota_scope__name",
                   "instance__service", "operation", "state", "instance", "date_submitted", "last_updated")
+
+    def render_id(self, value, record):
+        return format_html(f'<a title={value} href="{record.get_absolute_url()}">{record}</a>')
 
     def render_state(self, record, value):
         from service_catalog.views import map_request_state
-        return format_html(f'<a href="{record.get_absolute_url()}"><strong class="text-{ map_request_state(record.state) }">{ value }</strong></a>')
+        return format_html(
+            f'<a href="{record.get_absolute_url()}"><strong class="text-{map_request_state(record.state)}">{value}</strong></a>')
