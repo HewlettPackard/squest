3ef9946705ce74a4398d02e91ec8737f479f1556
elias-boulharts <elias.boulharts@hpe.com>
2023-09-18 12:03:01 +0200

    add quota scope verbose name in instance table

--- service_catalog/tables/instance_tables.py
+++ service_catalog/tables/instance_tables.py
@@ -1,6 +1,6 @@
 
 from django.utils.html import format_html
-from django_tables2 import TemplateColumn, LinkColumn, CheckBoxColumn
+from django_tables2 import TemplateColumn, LinkColumn, CheckBoxColumn, Column
 from django_tables2.utils import A
 
 from Squest.utils.squest_table import SquestTable
@@ -9,6 +9,7 @@ from service_catalog.models import Instance
 
 class InstanceTable(SquestTable):
     selection = CheckBoxColumn(accessor='pk', attrs={"th__input": {"onclick": "toggle(this)"}})
+    quota_scope__name = Column(verbose_name='Quota scope')
     name = LinkColumn()
     date_available = TemplateColumn(template_name='generics/custom_columns/generic_date_format.html')
     last_updated = TemplateColumn(template_name='generics/custom_columns/generic_date_format.html')
