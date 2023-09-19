64950ee8dd8fbe8af76d722e69a78e498edcc15f
Anthony Belhadj <abelhadj@hpe.com>
2023-09-19 14:49:39 +0200

    View role assignement in role if view_rbac

--- profiles/views/role.py
+++ profiles/views/role.py
@@ -2,7 +2,7 @@ from Squest.utils.squest_views import *
 from profiles.filters import RoleFilter
 from profiles.forms import RoleForm
 from profiles.models import Role
-from profiles.tables import RoleTable, PermissionTable
+from profiles.tables import RoleTable, PermissionTable, RoleAssignementUserTable, RoleAssignementScopeTable
 
 
 class RoleListView(SquestListView):
@@ -17,9 +17,11 @@ class RoleDetailView(SquestDetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        permission_table = PermissionTable(self.object.permissions.all())
+        permission_table = PermissionTable(self.object.permissions.prefetch_related("content_type"))
         permission_table.exclude = ("actions",)
         context['permissions_table'] = permission_table
+        context['rbac_assignement_user_table'] = RoleAssignementUserTable(self.object.get_role_assignment_user_dict())
+        context['rbac_assignement_scope_table'] = RoleAssignementScopeTable(self.object.get_role_assignment_scope_dict())
         return context
 
 
