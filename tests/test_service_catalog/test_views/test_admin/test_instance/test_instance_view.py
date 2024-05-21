71688632dd003fb16ea76385e798097ff58b1ad1
Nicolas Marcq <nico.marcq@gmail.com>
2024-05-21 17:02:04 +0200

    Feature: end user can rename instance or change owner

--- tests/test_service_catalog/test_views/test_admin/test_instance/test_instance_view.py
+++ tests/test_service_catalog/test_views/test_admin/test_instance/test_instance_view.py
@@ -3,6 +3,7 @@ from copy import copy
 
 from django.urls import reverse
 
+from profiles.models import Permission
 from service_catalog.models import Support, Instance, Request, InstanceState
 from tests.test_service_catalog.base_test_request import BaseTestRequest
 
@@ -66,6 +67,30 @@ class TestAdminInstanceViews(BaseTestRequest):
         self.assertEqual(302, response.status_code)
         self.test_instance.refresh_from_db()
         self.assertEqual(self.test_instance.name, "new_instance_name")
+
+    def test_instance_edit_standard_user(self):
+        self.client.login(username=self.standard_user, password=self.common_password)
+        url = reverse('service_catalog:instance_edit', kwargs=self.args)
+        response = self.client.post(url, data=self.edit_instance_data)
+        # by default is not allowed
+        self.assertEqual(403, response.status_code)
+        # give permission to the team
+        self.team_member_role.permissions.add(
+            Permission.objects.get_by_natural_key(codename="rename_instance",
+                                                  app_label="service_catalog",
+                                                  model="instance"),
+            Permission.objects.get_by_natural_key(codename="change_requester_on_instance",
+                                                  app_label="service_catalog",
+                                                  model="instance")
+        )
+        self.test_quota_scope.add_user_in_role(self.standard_user_2, self.team_member_role)
+        self.test_quota_scope_team.add_user_in_role(self.standard_user_2, self.team_member_role)
+        response = self.client.post(url, data=self.edit_instance_data)
+        # by default is not allowed
+        self.assertEqual(302, response.status_code)
+        self.test_instance.refresh_from_db()
+        self.assertEqual(self.test_instance.name, "new_instance_name")
+        self.assertEqual(self.test_instance.requester, self.standard_user_2)
 
     def test_instance_edit_with_empty_spec(self):
         old_spec = copy(self.test_instance.spec)
