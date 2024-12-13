5aab6668bf7847e5a432738dfea5f2e2e24beecc
Nicolas Marcq <nico.marcq@gmail.com>
2024-12-13 15:35:01 +0100

    add new test for bulk perm update on service and portfolio

--- service_catalog/models/portfolio.py
+++ service_catalog/models/portfolio.py
@@ -1,8 +1,12 @@
+import logging
+
 from django.db.models import ForeignKey, SET_NULL, CharField, ImageField
 from django.urls import reverse_lazy
 
 from Squest.utils.squest_model import SquestModel
+from profiles.models import Permission
 
+logger = logging.getLogger(__name__)
 
 class Portfolio(SquestModel):
     name = CharField(max_length=100)
@@ -36,5 +40,12 @@ class Portfolio(SquestModel):
         super(Portfolio, self).delete(using, keep_parents)
 
     def bulk_set_permission_on_operation(self, target_permission):
-        for service in self.service_list.all():
-            service.bulk_set_permission_on_operation(target_permission)
+        from service_catalog.models import Operation
+        logger.debug(f"Bulk edit permission on portfolio {self.name} to permission {target_permission}")
+        # check if all permission are already set to the target perm
+        all_permission_current_portfolio = Permission.objects.filter(operation__service__parent_portfolio=self).distinct()
+        # if the target perm is already the one used we do nothing
+        if all_permission_current_portfolio.count() == 1:
+            if all_permission_current_portfolio.first() == target_permission:
+                return
+        Operation.objects.filter(service__parent_portfolio=self).update(permission=target_permission)
---
+++ tests/test_service_catalog/test_models/test_service.py
@@ -0,0 +1,17 @@
+from service_catalog.forms import FormUtils
+from test_service_catalog.base_test_request import BaseTestRequest
+
+
+class TestService(BaseTestRequest):
+
+    def setUp(self):
+        super(TestService, self).setUp()
+
+    def test_bulk_set_permission_on_operation(self):
+        for current_perm in self.service_test.operations.values_list("permission", flat=True):
+            self.assertEqual(current_perm, FormUtils.get_default_permission_for_operation())
+
+        # apply new perm
+        self.service_test.bulk_set_permission_on_operation(self.admin_operation)
+        for current_perm in self.service_test.operations.values_list("permission", flat=True):
+            self.assertEqual(current_perm, self.admin_operation.id)
