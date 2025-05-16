529701964f3b7fdd72a1062029b0b9e501cca100
Nicolas Marcq <nico.marcq@gmail.com>
2025-05-16 16:04:10 +0200

    fix migration command in monitoring app

--- monitoring/apps.py
+++ monitoring/apps.py
@@ -2,12 +2,19 @@ import sys
 
 from django.apps import AppConfig
 import prometheus_client
+from django.db.models.signals import post_migrate
+
+
+def run_prometheus_metrics(sender, **kwargs):
+    from .models import ComponentCollector
+    prometheus_client.REGISTRY.register(ComponentCollector())
+
 
 class MonitoringConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'monitoring'
 
     def ready(self):
-        if 'manage.py' not in sys.argv and 'test' not in sys.argv:
-            from .models import ComponentCollector
-            prometheus_client.REGISTRY.register(ComponentCollector())
+        banned_words = ['test']
+        if not any(banned_word in sys.argv for banned_word in banned_words):
+            post_migrate.connect(run_prometheus_metrics, sender=self)
