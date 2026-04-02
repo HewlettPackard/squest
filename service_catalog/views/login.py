4b3a236632ca4e21d72cc07e6098d642e10fc1c8
Nicolas Marcq <nico.marcq@gmail.com>
2026-04-02 14:13:24 +0200

    refactor social authentication to support multiple backends
    Signed-off-by: Nicolas Marcq <nico.marcq@gmail.com>

--- service_catalog/views/login.py
+++ service_catalog/views/login.py
@@ -2,15 +2,19 @@ from django.contrib.auth import views as auth_views
 
 
 class LoginView(auth_views.LoginView):
-    template_name = 'registration/login.html'
+    template_name = "registration/login.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         from django.conf import settings
-        context.update({
-            'login_helper_text': settings.LOGIN_HELPER_TEXT,
-            'openid_active': settings.SOCIAL_AUTH_OIDC_ENABLED,
-            'openid_btn_text': settings.SOCIAL_AUTH_OIDC_BTN_TEXT,
-            'password_enabled': settings.PASSWORD_ENABLED
-        })
+
+        context.update(
+            {
+                "login_helper_text": settings.LOGIN_HELPER_TEXT,
+                "openid_active": settings.SOCIAL_AUTH_OIDC_ENABLED,
+                "openid_btn_text": settings.SOCIAL_AUTH_OIDC_BTN_TEXT,
+                "social_auth_backend": settings.SOCIAL_AUTH_SELECTED_BACKEND,
+                "password_enabled": settings.PASSWORD_ENABLED,
+            }
+        )
         return context
